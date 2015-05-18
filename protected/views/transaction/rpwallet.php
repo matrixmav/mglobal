1adcc36ed68b04ed63d20a55f38c91814df387c6
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-18 19:07:13 +0530

    updated

--- protected/views/transaction/rpwallet.php
+++ protected/views/transaction/rpwallet.php
@@ -4,6 +4,7 @@
 
 $this->breadcrumbs=array(
 	'Wallet Summery',
+        'RP Wallet',
 );
 
 $this->menu=array(
@@ -43,26 +44,26 @@ $this->menu=array(
 		array(
                     'name'=>'id',
                     'header'=>'<span style="white-space: nowrap;">Transfer To &nbsp; &nbsp; &nbsp;</span>',
-                    'value'=>'isset($data->touser->name)?$data->touser->name:""',
+                    'value'=>'isset($data->touser->name)? ucwords($data->touser->name):""',
 		),
             array(
                     'name'=>'id',
                     'header'=>'<span style="white-space: nowrap;">Transfer From &nbsp; &nbsp; &nbsp;</span>',
-                    'value'=>'isset($data->fromuser->name)?$data->fromuser->name:""',
+                    'value'=>'isset($data->fromuser->name)? ucwords($data->fromuser->name):""',
 		),
                 array(
                     'name'=>'transaction_id',
                     'header'=>'<span style="white-space: nowrap;">Paid Amount &nbsp; &nbsp; &nbsp;</span>',
-                    'value'=>'$data->transaction->paid_amount',
+                    'value'=>'isset($data->transaction->paid_amount)? number_format($data->transaction->paid_amount,2):""',
 		),
             array(
-                    'name'=>'domain',
+                    'name'=>'created_at',
                     'header'=>'<span style="white-space: nowrap;">Transfer Date &nbsp; &nbsp; &nbsp;</span>',
                     'value'=>'$data->created_at',
 		),
             
              array(
-                    'name'=>'domain',
+                    'name'=>'comment',
                     'header'=>'<span style="white-space: nowrap;">Comment &nbsp; &nbsp; &nbsp;</span>',
                     'value'=>'$data->comment',
 		),
