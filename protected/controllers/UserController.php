8b421793f130afcef2e5472738c489cede48e116
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-20 12:24:43 +0530

    updated

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -172,7 +172,26 @@ class UserController extends Controller
                 $modelGenealogy->user_id = $model->id ; 
                 $modelGenealogy->sponsor_user_id = $userObjectId->id;                 
                 $modelGenealogy->position = $_POST['position'];                 
-                $modelGenealogy->save(false); 
+                $modelGenealogy->save(false);
+                /*User entry in builder*/
+                
+                $builderObject = new WebsiteadminAdminUsers();
+                $builderObject->first_name = $_POST['full_name'] ;
+                $builderObject->username = $_POST['name'] ;
+                $builderObject->type = "Basic" ;
+                $builderObject->password = md5('12345');
+                $builderObject->save(false);
+                
+                /*User entry in builder templates*/
+                $buildertemplateObject = new WebsiteadminUserTemplates();
+                $buildertemplateObject->name = $_POST['full_name'];
+                $buildertemplateObject->user = $_POST['name'];
+                $buildertemplateObject->save(false);
+                
+                /*User entry in builder weblog*/
+                $builderweblogObject = new WebsiteadminWeblog();
+                $builderweblogObject->user = $_POST['name'];
+                $builderweblogObject->save(false);
                 $successMsg = "<p class='success'>You have successfully registered. Please check your email to activate your account</p>"; 
                 /*  For Genealogy Data */
                 
