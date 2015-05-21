54760a6e4ddaa0394bc91d41fc690df491c735b6
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-21 10:58:35 +0530

    updated

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'HK-Base',
+	'name'=>'MGlobal',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -109,7 +109,7 @@ return array(
 				'connectionString' => 'mysql:host=localhost;dbname=mglobal',
 				'emulatePrepare' => true,
 				'username' => 'root',
-				'password' => 'root',
+				'password' => '',
 				'charset' => 'utf8',
                                 'tablePrefix' => 'tbl_',
 		),
