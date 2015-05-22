0b05dddd62592728791f346cf25757680fbb5dcd
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-22 19:35:09 +0530

    update

--- protected/modules/admin/controllers/PackageController.php
+++ protected/modules/admin/controllers/PackageController.php
@@ -28,7 +28,7 @@ class PackageController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view','packageadd','packagedit'),
+				'actions'=>array('index','view','packageadd','packagedit','packagelist'),
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
@@ -130,10 +130,16 @@ class PackageController extends Controller
             
         }
             
-         
+         /*
+          * Function to fetch Package list
+          */
+         public function actionPackageList() {
+             
+             
+         }
 
-	 
-	/**
+         
+         /**
 	 * Returns the data model based on the primary key given in the GET variable.
 	 * If the data model is not found, an HTTP exception will be raised.
 	 * @param integer $id the ID of the model to be loaded
