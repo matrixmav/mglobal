667b5830b3845e779efa7f509116c9bfe1d8638f
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-22 19:34:43 +0530

    update

---
+++ protected/modules/admin/views/package/package_list.php
@@ -0,0 +1,130 @@
+<?php
+/* @var $this UserController */
+/* @var $model User */
+
+$this->breadcrumbs = array(
+    'Reports' => array('/admin/package/packagelist'),'Package List'
+);
+?>
+
+<style>
+    .confirmBtn{left: 333px;
+    position: absolute;
+    top: 0;}
+    
+    .confirmOk{left: 610px;
+    position: absolute;
+    top: 8px;}
+    .confirmMenu{position: relative;}
+</style>
+<div class="col-md-12">
+    
+        <div class="expiration margin-topDefault confirmMenu">
+                    
+    <form id="regervation_filter_frm" name="regervation_filter_frm" method="post" action="/admin/report/package">
+    <div class="input-group input-large date-picker input-daterange">
+        <input type="text" name="from" placeholder="To Date" class="datepicker form-control">
+        <span class="input-group-addon">
+        to </span>
+        <input type="text" name="to" data-provide="datepicker" placeholder="From Date" class="datepicker form-control">
+    </div>
+    <?php 
+    $statusId =   1;
+    if(isset($_REQUEST['res_filter'])){
+      $statusId =   $_REQUEST['res_filter'];
+    } ?>
+    
+    <select class="customeSelect howDidYou form-control input-medium select2me confirmBtn" id="ui-id-5" name="res_filter">
+                <option value="1" <?php if($statusId == 1){ echo "selected"; } ?> >Active</option>
+                <option value="0" <?php if($statusId == 3){ echo "selected"; } ?> >In Active</option>
+            </select>
+    </div>
+    <input type="submit" class="btn btn-primary confirmOk" value="OK" name="submit" id="submit"/>
+    </form>
+
+</div>
+<div class="row">
+    <div class="col-md-12">
+        <?php
+        $this->widget('zii.widgets.grid.CGridView', array(
+            'id' => 'state-grid',
+            'dataProvider' => $dataProvider,
+            'enableSorting' => 'true',
+            'ajaxUpdate' => true,
+            'summaryText' => 'Showing {start} to {end} of {count} entries',
+            'template' => '{items} {summary} {pager}',
+            'itemsCssClass' => 'table table-striped table-bordered table-hover table-full-width',
+            'pager' => array(
+                'header' => false,
+                'firstPageLabel' => "<<",
+                'prevPageLabel' => "<",
+                'nextPageLabel' => ">",
+                'lastPageLabel' => ">>",
+            ),
+            'columns' => array(
+                //'idJob',
+                array(
+                    'name' => 'name',
+                    'header' => '<span style="white-space: nowrap;">Name &nbsp; &nbsp; &nbsp;</span>',
+                    'value' => '$data->name',
+                ),
+                 array(
+                    'name' => 'amount',
+                    'header' => '<span style="white-space: nowrap;">Amount &nbsp; &nbsp; &nbsp;</span>',
+                    'value' => '$data->amount',
+                ),
+                array(
+                    'name' => 'start_date',
+                    'header' => '<span style="white-space: nowrap;">Start Date&nbsp; &nbsp; &nbsp;</span>',
+                    'value' => '$data->start_date',
+                ),
+                array(
+                    'name' => 'end_date',
+                    'header' => '<span style="white-space: nowrap;">End Date &nbsp; &nbsp; &nbsp;</span>',
+                    'value' => '$data->end_date',
+                ),
+                
+                array(
+                    'name' => 'Description',
+                    'header' => '<span style="white-space: nowrap;">Description &nbsp; &nbsp; &nbsp;</span>',
+                    'value' => '$data->Description',
+                ),
+                array(
+                    'name' => 'status',
+                    'value' => '($data->status == 1) ? Yii::t(\'translation\', \'Active\') : Yii::t(\'translation\', \'Inactive\')',
+                ),
+                array(
+                    'class' => 'CButtonColumn',
+                    'template' => '{Change}',
+                    'htmlOptions' => array('width' => '30%'),
+                    'buttons' => array(
+//                        'Edit' => array(
+//                            'label' => 'Edit',
+//                            'options' => array('class' => 'btn purple fa fa-edit margin-right15'),
+//                            'url' => 'Yii::app()->createUrl("admin/state/update", array("id"=>$data->id))',
+//                        ),
+                        'Change' => array(
+                            'label' => Yii::t('translation', 'Change Status'),
+                            'options' => array('class' => 'fa fa-success btn default black delete'),
+                            'url' => 'Yii::app()->createUrl("admin/user/changestatus", array("id"=>$data->id))',
+                        ),
+                        
+                      'Change' => array(
+                            'label' => Yii::t('translation', 'Change Status'),
+                            'options' => array('class' => 'fa fa-success btn default black delete'),
+                            'url' => 'Yii::app()->createUrl("admin/user/changestatus", array("id"=>$data->id))',
+                        ),
+                        'Change' => array(
+                            'label' => Yii::t('translation', 'Change Status'),
+                            'options' => array('class' => 'fa fa-success btn default black delete'),
+                            'url' => 'Yii::app()->createUrl("admin/user/changestatus", array("id"=>$data->id))',
+                        ),
+                        
+                        
+                    ),
+                ),
+            ),
+        ));
+        ?>
+    </div>
+</div>
