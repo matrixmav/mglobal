b9548ea2172046054822c4c396850d61b89952e4
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-25 18:56:18 +0530

    update

--- protected/views/layouts/inner.php
+++ protected/views/layouts/inner.php
@@ -336,6 +336,7 @@ License: You must have a valid license purchased only from themeforest(the above
                             if ((in_array($hotel_pmenu, $menusections ['psections'])) || (in_array($hotel_pmenu, $menusections ['section_ids']))) {
                                 $hotel_subsection = array(
                                     "order/list" => "My Order",
+                                    "profile/changepassword" => "Change Password",
                                     "profile/updateprofile" => "Profile",
                                     "profile/address" => "Address",
                                     "profile/documentverification" => "Verification",
