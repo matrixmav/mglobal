a32e0485179020953c34e3dab7ce1f069e54f15d
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-25 16:04:20 +0530

    update

--- protected/config/main.php
+++ protected/config/main.php
@@ -9,6 +9,7 @@ return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 
 	'name'=>'MGlobal',
+        'URL'=>'http://localhost/',
 	// preloading 'log' component
 	'preload'=>array('log'),
 
