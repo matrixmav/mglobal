c471db610fe3181eed8116a705a2fa1d50c254b5
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-25 19:16:01 +0530

    update

---
+++ protected/views/user/change_password.php
@@ -0,0 +1,92 @@
+<?php
+$this->breadcrumbs = array(
+    'Account' => array('profile/updateprofile'),
+    'Change Password',
+);
+
+?>
+
+<div class="col-md-7 col-sm-7">
+    <div class="error" id="error_msg" style="display: none;"></div>
+    <?php if($error){?><div class="error" id="error_msg"><?php echo $error;?></div><?php }?>
+<?php if($success){?><div class="success" id="error_msg"><?php echo $success;?></div><?php }?>
+    <form action="/profile/changepassword" method="post" class="form-horizontal" onsubmit="return validation();">
+     
+        <fieldset>
+            <legend>Change Password</legend>
+             <div class="form-group">
+                <label class="col-lg-4 control-label" for="lastname">Old Password<span class="require">*</span></label>
+                <div class="col-lg-8">
+                    <input type="password" id="old_password" class="form-control" name="UserProfile[old_password]">
+                    <div id="old_error_msg"></div>
+                </div>
+            </div>
+             <div class="form-group">
+                <label class="col-lg-4 control-label" for="lastname">New Password<span class="require">*</span></label>
+                <div class="col-lg-8">
+                    <input type="password" id="new_password" class="form-control" name="UserProfile[new_password]">
+                    <div id="new_error_msg"></div>
+                </div>
+            </div>
+             <div class="form-group">
+                <label class="col-lg-4 control-label" for="lastname">Confirm Password<span class="require">*</span></label>
+                <div class="col-lg-8">
+                    <input type="password" id="confirm_password" class="form-control" name="UserProfile[confirm_password]">
+                    <div id="confirm_error_msg"></div>
+                </div>
+            </div>
+             
+            <div class="form-group">
+                <label class="col-lg-4 control-label" for="lastname">Master Pin<span class="require">*</span></label>
+                <div class="col-lg-8">
+                    <input type="password" id="master_pin" class="form-control" name="UserProfile[master_pin]">
+                    <div id="master_error_msg"></div>
+                </div>
+            </div>
+        </fieldset>
+
+    <div class="row">
+            <div class="col-lg-8 col-md-offset-4 padding-left-0 padding-top-20">                      
+                <input type="submit" name="submit" value="Change" class="btn red">
+                 
+            </div>
+        </div>
+    </form>
+</div>
+<script>
+     function validation()
+    {
+        if($("#old_password").val()=='')
+        {
+            $("#old_error_msg").html("Please enter your old password.");
+            $("#old_password").focus();
+            return false;
+        }
+         if($("#new_password").val()=='')
+        {
+            $("#new_error_msg").html("Please enter your new password.");
+            $("#new_password").focus();
+            return false;
+        }
+        if($("#confirm_password").val()=='')
+        {
+            $("#confirm_error_msg").html("Please enter your confirm password.");
+            $("#confirm_password").focus();
+            return false;
+        }
+        if($("#confirm_password").val() != $("#new_password").val())
+        {
+            $("#confirm_error_msg").html("New password and confirm password must be same.");
+            $("#confirm_password").focus();
+            return false;
+        }
+        if($("#master_pin").val()=='')
+        {
+            $("#master_error_msg").html("Please enter your confirm password.");
+            $("#master_pin").focus();
+            return false;
+        }
+    }
+</script>
+
+
