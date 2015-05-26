160eef269a00ed963122a370bf706ab91db16927
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-26 11:31:35 +0530

    update

--- protected/modules/admin/controllers/BuildTempController.php
+++ protected/modules/admin/controllers/BuildTempController.php
@@ -1,111 +1,157 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
- 
+class BuildTempController extends Controller {
 
-class BuildTempController extends Controller
-{
-   
-	/**
-	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
-	 * using two-column layout. See 'protected/views/layouts/column2.php'.
-	 */
-	public $layout='main';
+    /**
+     * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+     * using two-column layout. See 'protected/views/layouts/column2.php'.
+     */
+    public $layout = 'main';
 
-	/**
-	 * @return array action filters
-	 */
-	public function filters()
-	{
-		return array(
-			'accessControl', // perform access control for CRUD operations
-			'postOnly + delete', // we only allow deletion via POST request
-		);
-	}
+    /**
+     * @return array action filters
+     */
+    public function filters() {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+            'postOnly + delete', // we only allow deletion via POST request
+        );
+    }
 
-	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
-	public function accessRules()
-	{
-		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','categoryadd','categoryedit'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
-		);
-	}	
-    
-    
-     public function actionIndex()
-	{
-		$this->render('index');
-	}
-    
-        public function actionCategoryAdd() 
-        {
-            $error = "";
-            $success = "";
-         $categoryObject = new BuildCategory;   
-         if(!empty($_POST))
-         {
-             
-             
-         }else{
-          $error .= "Please fill all required(*) marked fields";  
-         }     
-         $this->render('category_add',array('$error'=>$error,'success'=>$success)); 
-        } 
-        
-        public function functionName($param) {
-            
-        $categoryObject = BuildCategory::model()->findByPk($_REQUEST['id']); 
-        
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules() {
+        return array(
+            array('allow', // allow all users to perform 'index' and 'view' actions
+                'actions' => array('index', 'categoryadd', 'categoryedit', 'categorylist'),
+                'users' => array('*'),
+            ),
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                'actions' => array('create', 'update'),
+                'users' => array('@'),
+            ),
+            array('allow', // allow admin user to perform 'admin' and 'delete' actions
+                'actions' => array('admin', 'delete'),
+                'users' => array('admin'),
+            ),
+            array('deny', // deny all users
+                'users' => array('*'),
+            ),
+        );
+    }
+
+    public function actionIndex() {
+        $this->render('index');
+    }
+
+    /*
+     * function to add builder category
+     */
+
+    public function actionCategoryAdd() {
+        $error = "";
+        $success = "";
+        $categoryObject = new BuildCategory;
+        if ($_POST) {
+            if ($_POST['Category']['name'] != '') {
+                $categoryObject->name = $_POST['Category']['name'];
+                $categoryObject->created_at = date('Y-m-d');
+                if ($categoryObject->save(false)) {
+                    $this->redirect(array('categorylist', 'success' => $success));
+                }
+            } else {
+                $error .= "Please fill all required(*) marked fields";
+            }
         }
-        
+        $this->render('/builder/category_add', array('error' => $error, 'success' => $success));
+    }
 
-	// Uncomment the following methods and override them if needed
-	/*
-	public function filters()
-	{
-		// return the filter configuration for this controller, e.g.:
-		return array(
-			'inlineFilterName',
-			array(
-				'class'=>'path.to.FilterClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
+    /*
+     * Function to edit bilder category
+     */
 
-	public function actions()
-	{
-		// return external action classes, e.g.:
-		return array(
-			'action1'=>'path.to.ActionClass',
-			'action2'=>array(
-				'class'=>'path.to.AnotherActionClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-	*/
+    public function actionCategoryEdit() {
+        $error = "";
+        $success = "";
+        if ($_REQUEST['id']) {
+            $categoryObject = BuildCategory::model()->findByPk($_REQUEST['id']);
+            if ($_POST) {
+                if ($_POST['Category']['name'] != '') {
+                    $categoryObject->name = $_POST['Category']['name'];
+                    $categoryObject->created_at = date('Y-m-d');
+                    if ($categoryObject->save(false)) {
+                        $this->redirect(array('categorylist', 'success' => $success));
+                    }
+                } else {
+                    $error .= "Please fill all required(*) marked fields";
+                }
+            }
+        }
+        $this->render('/builder/category_edit', array('error' => $error, 'success' => $success));
+    }
+
+    /*
+     * Function to list bilder category
+     */
+
+    public function actionCategoryList() {
+        $error = "";
+        $success = "";
+        if ($_REQUEST['id']) {
+            $categoryObject = BuildCategory::model()->findByPk($_REQUEST['id']);
+            if ($_POST) {
+                if ($_POST['Category']['name'] != '') {
+                    $categoryObject->name = $_POST['Category']['name'];
+                    $categoryObject->created_at = date('Y-m-d');
+                    if ($categoryObject->save(false)) {
+                        $this->redirect(array('categorylist', 'success' => $success));
+                    }
+                } else {
+                    $error .= "Please fill all required(*) marked fields";
+                }
+            }
+        }
+        $this->render('/builder/category_edit', array('error' => $error, 'success' => $success));
+    }
+
+    public function functionName($param) {
+
+        $categoryObject = BuildCategory::model()->findByPk($_REQUEST['id']);
+    }
+
+    // Uncomment the following methods and override them if needed
+    /*
+      public function filters()
+      {
+      // return the filter configuration for this controller, e.g.:
+      return array(
+      'inlineFilterName',
+      array(
+      'class'=>'path.to.FilterClass',
+      'propertyName'=>'propertyValue',
+      ),
+      );
+      }
+
+      public function actions()
+      {
+      // return external action classes, e.g.:
+      return array(
+      'action1'=>'path.to.ActionClass',
+      'action2'=>array(
+      'class'=>'path.to.AnotherActionClass',
+      'propertyName'=>'propertyValue',
+      ),
+      );
+      }
+     */
 }
