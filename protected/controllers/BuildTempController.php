df2b1fb70bccc9b57abf08d42cbf35bfb1504095
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-26 14:03:56 +0530

    update

--- protected/controllers/BuildTempController.php
+++ protected/controllers/BuildTempController.php
@@ -2,10 +2,23 @@
 
 class BuildTempController extends Controller
 {
-	public function actionIndex()
+   
+	/**
+	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+	 * using two-column layout. See 'protected/views/layouts/column2.php'.
+	 */
+	public $layout='inner';
+
+ 	
+    
+    
+     public function actionIndex()
 	{
 		$this->render('index');
 	}
+    
+             
+        
 
 	// Uncomment the following methods and override them if needed
 	/*
