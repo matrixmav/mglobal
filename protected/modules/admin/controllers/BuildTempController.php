47d631ea294d7834055c88c494bde870cdd39606
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-26 11:16:10 +0530

    update

---
+++ protected/modules/admin/controllers/BuildTempController.php
@@ -0,0 +1,102 @@
+<?php
+
+class BuildTempController extends Controller
+{
+   
+	/**
+	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+	 * using two-column layout. See 'protected/views/layouts/column2.php'.
+	 */
+	public $layout='main';
+
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+			'postOnly + delete', // we only allow deletion via POST request
+		);
+	}
+
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+			array('allow',  // allow all users to perform 'index' and 'view' actions
+				'actions'=>array('index','categoryadd','categoryedit'),
+				'users'=>array('*'),
+			),
+			array('allow', // allow authenticated user to perform 'create' and 'update' actions
+				'actions'=>array('create','update'),
+				'users'=>array('@'),
+			),
+			array('allow', // allow admin user to perform 'admin' and 'delete' actions
+				'actions'=>array('admin','delete'),
+				'users'=>array('admin'),
+			),
+			array('deny',  // deny all users
+				'users'=>array('*'),
+			),
+		);
+	}	
+    
+    
+     public function actionIndex()
+	{
+		$this->render('index');
+	}
+    
+        public function actionCategoryAdd() 
+        {
+            $error = "";
+         $categoryObject = new BuildCategory;   
+         if(!empty($_POST))
+         {
+             
+             
+         }else{
+          $error .= "Please fill all required(*) marked fields";  
+         }     
+         $this->render('category_add',array('$error'=>$error,'success'=>$success)); 
+        } 
+        
+        public function functionName($param) {
+            
+        $categoryObject = BuildCategory::model()->findByPk($_REQUEST['id']); 
+        
+        }
+        
+
+	// Uncomment the following methods and override them if needed
+	/*
+	public function filters()
+	{
+		// return the filter configuration for this controller, e.g.:
+		return array(
+			'inlineFilterName',
+			array(
+				'class'=>'path.to.FilterClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+
+	public function actions()
+	{
+		// return external action classes, e.g.:
+		return array(
+			'action1'=>'path.to.ActionClass',
+			'action2'=>array(
+				'class'=>'path.to.AnotherActionClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+	*/
+}
