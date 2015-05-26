13388f572fc0873c4751a623f3e5240ce2836813
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-26 16:51:09 +0530

    update

--- protected/modules/admin/controllers/BuildTempController.php
+++ protected/modules/admin/controllers/BuildTempController.php
@@ -257,6 +257,69 @@ class BuildTempController extends Controller {
             $headeraddObject->template_title = $_POST['Template']['template_title'];
             $headeraddObject->created_at = date('Y-m-d');
             if ($headeraddObject->save(false)) {
+                   function rmdir_recursive($dir) {
+                    foreach (scandir($dir) as $file) {
+                        if ('.' === $file || '..' === $file)
+                            continue;
+                        if (is_dir("$dir/$file"))
+                            rmdir_recursive("$dir/$file");
+                        else
+                            unlink("$dir/$file");
+                    }
+                    rmdir($dir);
+                }
+
+                if ($_FILES["cssfolder"]["name"]) {
+                    $filename = $_FILES["cssfolder"]["name"];
+                    $source = $_FILES["cssfolder"]["tmp_name"];
+                    $type = $_FILES["cssfolder"]["type"];
+
+                    $name = explode(".", $filename);
+                    $accepted_types = array('application/zip', 'application/x-zip-compressed', 'multipart/x-zip', 'application/x-compressed');
+                    foreach ($accepted_types as $mime_type) {
+                        if ($mime_type == $type) {
+                            $okay = true;
+                            break;
+                        }
+                    }
+
+                    $continue = strtolower($name[1]) == 'zip' ? true : false;
+                    if (!$continue) {
+                        $error .= "The file you are trying to upload is not a .zip file. Please try again.";
+                    }
+
+                    /* PHP current path */
+                    $path = Yii::getPathOfAlias('webroot') . "/user/template/"; // absolute path to the directory where zipper.php is in
+                    $filenoext = basename($filename, '.zip');  // absolute path to the directory where zipper.php is in (lowercase)
+                    $filenoext = basename($filenoext, '.ZIP');  // absolute path to the directory where zipper.php is in (when uppercase)
+                    $fname = time() . $filenoext;
+                    $targetdir = $path . time() . $filenoext; // target directory
+                    $targetzip = $path . time() . $filename; // target zip file
+
+                    /* create directory if not exists', otherwise overwrite */
+                    /* target directory is same as filename without extension */
+
+                    if (is_dir($targetdir))
+                        rmdir_recursive($targetdir);
+                    mkdir($targetdir, 0777);
+
+                    /* here it is really happening */
+
+                    if (move_uploaded_file($source, $targetzip)) {
+                        $zip = new ZipArchive();
+                        $x = $zip->open($targetzip);  // open the zip file to extract
+                        if ($x === true) {
+                            $zip->extractTo($targetdir); // place in the directory with same name  
+                            $zip->close();
+
+                            unlink($targetzip);
+                        }
+                        $message = "Your .zip file was uploaded and unpacked.";
+                    } else {
+                        $message = "There was a problem with the upload. Please try again.";
+                    }
+                }
+                 
                 $model = new BuildTemp;
                 $model->temp_header_id = $headeraddObject->id;
                 $model->temp_body_id = 0;
@@ -265,6 +328,7 @@ class BuildTempController extends Controller {
                 $model->created_at = date('Y-m-d');
                 $model->updated_at = date('Y-m-d');
                 $model->category_id = $category;
+                $model->folderpath = $headeraddObject->$filenoext;
                 $model->save(false);
                 $success .= "Header content added successfully";
             }
