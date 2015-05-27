6e75266467896d71bf707fa402df39014cb42cea
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-27 19:46:11 +0530

    update

--- protected/views/buildTemp/editheader.php
+++ protected/views/buildTemp/editheader.php
@@ -2,14 +2,3 @@
 <textarea name="Header[header_content]" id="editor1"><?php echo $builderObject->temp_header; ?></textarea> 
 <input type="submit" name="submit" value="Submit">
 </form>  
-<script type="text/javascript">
-    CKEDITOR.replace( 'editor1' , {
-    filebrowserBrowseUrl : '/ckfinder/ckfinder.html',
-    filebrowserImageBrowseUrl : '/ckfinder/ckfinder.html?type=Images',
-    filebrowserFlashBrowseUrl : '/ckfinder/ckfinder.html?type=Flash',
-    filebrowserUploadUrl : '/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files',
-    filebrowserImageUploadUrl : '/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images',
-    filebrowserFlashUploadUrl : '/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash'
-});
-CKFinder.setupCKEditor( editor, '../' );
-</script>
--- protected/views/buildTemp/userinput.php
+++ protected/views/buildTemp/userinput.php
@@ -7,27 +7,27 @@ $this->breadcrumbs = array(
 <div class="col-md-7 col-sm-7" id="test">
     <?php if($error){?><div class="error" id="error_msg"><?php echo $error;?></div><?php }?>
     <?php if($success){?><div class="success" id="error_msg"><?php echo $success;?></div><?php }?>
-    <?php if(!empty($userpagesObject)){?>
-    <?php foreach($userpagesObject as $page){?><a href="/buildtemp/pagedit?page_id=<?php echo $page->id; ?>"><?php echo $page->page_name; ?></a><?php }?>
-    <?php }else{ ?>
-    <a href="/buildtemp/userinput">Page1</a>&nbsp;&nbsp;<a href="/buildtemp/userinput">Page2</a>&nbsp;&nbsp;<a href="/buildtemp/userinput">Page3</a>&nbsp;&nbsp;<a href="/buildtemp/userinput">Page4</a>
-    <?php }?>
-    <form action="/admin/buildtemp/userinput" method="post" class="form-horizontal" onsubmit="return validation();" enctype="multipart/form-data">
+     <?php if(count($userpagesObject) < 4) {?>
+    <a href="/buildtemp/userinput">Add page</a>
+     <?php }else{
+      foreach($userpagesObject as $page){?><a href="/buildtemp/pagedit?page_id=<?php echo $page->id; ?>"><?php echo $page->page_name; ?></a><?php }?>   
+      <?php }?>
+    <form action="/buildtemp/userinput" method="post" class="form-horizontal" onsubmit="return validation();" enctype="multipart/form-data">
      
         <fieldset>
             <legend>Add Pages</legend>
              <div class="form-group">
                 <label class="col-lg-4 control-label" for="lastname">Page Title<span class="require">*</span></label>
                 <div class="col-lg-8">
-                    <input id="template_title" type="text" class="form-control" name="pages[template_title]">
-                    <span id="template_title_error"></span>
+                    <input id="page_name" type="text" class="form-control" name="pages[page_name]">
+                    <span id="page_title_error"></span>
                 </div>
             </div>
             <div class="form-group">
-                <label class="col-lg-4 control-label" for="lastname">Header Code<span class="require">*</span></label>
+                <label class="col-lg-4 control-label" for="lastname">Page Content<span class="require">*</span></label>
                 <div class="col-lg-8">
                     <textarea id="editor1" class="form-control" name="pages[page_content]" style="width: 482px; height: 248px;"></textarea>
-                    <span id="header_code_error"></span>
+                    <span id="page_content_error"></span>
                 </div>
             </div>
             
@@ -48,53 +48,31 @@ $this->breadcrumbs = array(
 <script type="text/javascript">
     function validation()
     {
-      $("#cssfolder_error").html("");
-      if ($("#cssfolder").val() == "") {
-      $("#cssfolder_error").html("Please upload css zip folder.");
-      $("#cssfolder").focus();            
+      $("#page_title_error").html("");
+      if ($("#page_name").val() == "") {
+      $("#page_title_error").html("Please enter page title.");
+      $("#page_name").focus();            
       return false;
     }
     
-    $("#screenshot_error").html("");
-      if ($("#screenshot").val() == "") {
-      $("#screenshot_error").html("Please upload template screenshot.");
-      $("#screenshot").focus();            
+    $("#page_content_error").html("");
+      if ($("#editor1").val() == "") {
+      $("#page_content_error").html("Please enter page content.");
+      $("#editor1").focus();            
       return false;
     }
-     
-      $("#category_error").html("");
-      if ($("#category").val() == "") {
-      $("#category_error").html("Please select template category");
-      $("#category").focus();            
-      return false;
-    }
-     $("#template_title_error").html("");
-      if($("#template_title").val() == "") {
-      $("#template_title_error").html("Please enter template title");
-      $("#template_title").focus();            
-      return false;
-    }
-      $("#header_code_error").html("");
-      if ($("#header_code").val() == "") {
-      $("#header_code_error").html("Please enter Header Code");
-      $("#header_code").focus();            
-      return false;
-    }
-    $("#header_code_error").html("");
-      if ($("#body_code").val() == "") {
-      $("#header_code_error").html("Enter Body Code");
-      $("#body_code").focus();            
-      return false;
-    }
-     $("#header_code_error").html("");
-      if ($("#footer_code").val() == "") {
-      $("#header_code_error").html("Enter Footer Code");
-      $("#footer_code").focus();            
-      return false;
-    }
-     
-    }
+   }
     
     </script>
-     
+       <script type="text/javascript">
+    CKEDITOR.replace( 'editor1' , {
+    filebrowserBrowseUrl : '/ckfinder/ckfinder.html',
+    filebrowserImageBrowseUrl : '/ckfinder/ckfinder.html?type=Images',
+    filebrowserFlashBrowseUrl : '/ckfinder/ckfinder.html?type=Flash',
+    filebrowserUploadUrl : '/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files',
+    filebrowserImageUploadUrl : '/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images',
+    filebrowserFlashUploadUrl : '/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash'
+});
+CKFinder.setupCKEditor( editor, '../' );
+</script>
      
