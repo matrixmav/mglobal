d5856dde702f166f6750de2052fdfb4d67f8696d
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-27 14:24:27 +0530

    update

---
+++ protected/views/buildTemp/editheader.php
@@ -0,0 +1,6 @@
+<form action="">
+<textarea name="header" id="editor1"><?php echo $builderObject->header->header_content; ?></textarea>    
+</form>  
+<script type="text/javascript">
+    CKEDITOR.replace( 'editor1' );
+</script>
