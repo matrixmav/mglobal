1358ff08cf0f26a3c2d3853c6a629e344606caa2
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-27 16:56:41 +0530

    update

--- protected/views/buildTemp/editheader.php
+++ protected/views/buildTemp/editheader.php
@@ -3,5 +3,13 @@
 <input type="submit" name="submit" value="Submit">
 </form>  
 <script type="text/javascript">
-    CKEDITOR.replace( 'editor1' );
+    CKEDITOR.replace( 'editor1' , {
+    filebrowserBrowseUrl : '/ckfinder/ckfinder.html',
+    filebrowserImageBrowseUrl : '/ckfinder/ckfinder.html?type=Images',
+    filebrowserFlashBrowseUrl : '/ckfinder/ckfinder.html?type=Flash',
+    filebrowserUploadUrl : '/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files',
+    filebrowserImageUploadUrl : '/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images',
+    filebrowserFlashUploadUrl : '/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash'
+});
+CKFinder.setupCKEditor( editor, '../' );
 </script>
