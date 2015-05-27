8c42f71b7f7572bb4f3846f50532c3f4c7027fd1
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-27 10:51:03 +0530

    update

--- protected/views/buildTemp/templates.php
+++ protected/views/buildTemp/templates.php
@@ -1,4 +1,8 @@
 <?php foreach($builderObject as $buildertemp){?>
 <a href=""><img src="/user/template/<?php echo $buildertemp->folderpath;?>/screenshot/<?php echo $buildertemp->screenshot;?>"></a><br/>
-<a href="/buildtemp/managewebsite">Get Started</a>
+<form action="/buildtemp/managewebsite" method="post">
+<input type="hidden" name="user_id" id="user_id" value="<?php echo Yii::app()->session['package_id'];?>">
+<input type="hidden" name="template_id" id="template_id" value="<?php echo $buildertemp->template_id;?>">
+<input type="submit" name="submit" id="submit" value="Get Started">
+</form>
 <?php } ?>
