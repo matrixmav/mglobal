e9c37ae7f9c3b6840ecb853ae0b2acc40751eab1
surya.asati <surya.asati@MGIS-PC0031.MGIS.COM>
2015-05-28 19:14:42 +0530

    Merge branch 'master' of https://github.com/matrixmav/mglobal
    Conflicts:
    	protected/config/main.php
    	protected/controllers/UserController.php

--- sql/alter.sql
+++ sql/alter.sql
@@ -3,3 +3,7 @@ ALTER TABLE `genealogy` ADD `order_status` INT(11) NOT NULL AFTER `status`, ADD 
 ALTER TABLE `genealogy` CHANGE `updated_at` `updated_at` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP; 
 
 ALTER TABLE `user` ADD `unique_id` VARCHAR(255) NOT NULL AFTER `master_pin`;
+
+ALTER TABLE `ads` ADD `banner` VARCHAR(255) NOT NULL AFTER `name`;
+
+ALTER TABLE `ads` ADD `get_code` TEXT NOT NULL AFTER `status`;
