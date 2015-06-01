2bdf8f3d3d71ebb6c0e52297ce98e68b4e18e459
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-06-01 19:39:49 +0530

    update

--- protected/controllers/PackageController.php
+++ protected/controllers/PackageController.php
@@ -28,7 +28,7 @@ class PackageController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view','domainsearch','availabledomain','checkout','domainadd','productcart','couponapply','loaddomain','orderadd'),
+				'actions'=>array('index','view','domainsearch','availabledomain','checkout','domainadd','productcart','couponapply','loaddomain','orderadd','thankyou'),
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
@@ -527,5 +527,15 @@ class PackageController extends Controller
 			Yii::app()->end();
 		}
 	}
+        
+        /*
+         * function to save payment user select
+         */
+        
+        public function actionThankYou()
+        {
+            echo "nidhi"; exit;
+            
+        }
          
 }
