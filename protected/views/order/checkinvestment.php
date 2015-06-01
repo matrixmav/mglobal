e475f211a315fff41a5ca8527697443b0a2b6f5c
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-06-01 15:36:32 +0530

    update

---
+++ protected/views/order/checkinvestment.php
@@ -0,0 +1,90 @@
+<?php
+/* @var $this OrderController */
+/* @var $dataProvider CActiveDataProvider */
+
+$this->breadcrumbs = array(
+    'Summary' => array('summary/checkinvestment'),
+    'Check Investment',
+);
+ 
+
+$this->menu=array(
+	array('label'=>'Create Order', 'url'=>array('create')),
+	array('label'=>'Manage Order', 'url'=>array('admin')),
+);
+ 
+?>
+    <?php //echo "<pre>"; print_r($orderObject);?>
+       <div class="main">
+      <div class="container">
+
+        <!-- BEGIN SIDEBAR & CONTENT -->
+        <div class="row margin-bottom-40">
+         <!-- BEGIN SIDEBAR -->
+          <!-- END SIDEBAR -->
+          <!-- BEGIN CONTENT -->
+          <div class="col-md-10 col-sm-9">
+       
+        <?php 
+       echo "<pre>";print_r($dataProvider);
+        $fullname; exit;
+        $this->widget('zii.widgets.grid.CGridView', array(
+	'id'=>'city-grid',
+	'dataProvider'=>$dataProvider,
+	'enableSorting'=>'true',
+	'ajaxUpdate'=>true,
+	'summaryText'=>'Showing {start} to {end} of {count} entries',
+	'template'=>'{items} {summary} {pager}',
+	'itemsCssClass'=>'table table-striped table-bordered table-hover table-full-width',
+	'pager'=>array(
+		'header'=>false,
+		'firstPageLabel' => "<<",
+		'prevPageLabel' => "<",
+		'nextPageLabel' => ">",
+		'lastPageLabel' => ">>",
+	),	
+	'columns'=>array(
+		//'idJob',
+                array(
+                    'name'=>'id',
+                    'header'=>'<span style="white-space: nowrap;">Sl. No &nbsp; &nbsp; &nbsp;</span>',
+                    'value'=>'$row+1',
+		),
+		array(
+                    'name'=>'id',
+                    'header'=>'<span style="white-space: nowrap;">Name &nbsp; &nbsp; &nbsp;</span>',
+                    'value'=>'isset($data->full_name)? ucwords($data->full_name):""',
+		),
+                
+               array(
+                    'name'=>'id',
+                    'header'=>'<span style="white-space: nowrap;">Name &nbsp; &nbsp; &nbsp;</span>',
+                    'value'=>'isset($data->package_id)? $data->package_id:""',
+		),
+                /*array(
+                    'name'=>'transaction_id',
+                    'header'=>'<span style="white-space: nowrap;">Paid Amount &nbsp; &nbsp; &nbsp;</span>',
+                    'value'=>'isset($data->transaction->paid_amount)? number_format($data->transaction->paid_amount,2):""',
+		),
+            
+               array(
+                    'name'=>'status',
+                    'header'=>'<span style="white-space: nowrap;">Payment Status &nbsp; &nbsp; &nbsp;</span>',
+                    'value'=>'($data->transaction->status == 1) ? "Completed" : "Pending"',
+		),
+            
+               array(
+                    'name'=>'created_at',
+                    'header'=>'<span style="white-space: nowrap;">Created At &nbsp; &nbsp; &nbsp;</span>',
+                    'value'=>'$data->transaction->created_at',
+		),*/   
+            
+            
+             
+		 
+	),
+)); ?>
+                    
+
+      </div>
+    </div>
