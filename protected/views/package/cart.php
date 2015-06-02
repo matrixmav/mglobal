c8b7d6ed0ceb567a69529796ae66526ebc51b1d0
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-06-02 11:54:57 +0530

    update

--- protected/views/package/cart.php
+++ protected/views/package/cart.php
@@ -126,146 +126,162 @@
                         <a id="summary-btn" class="btn-flat-green xxl" onclick="proceedPayment();">Proceed to Payment</a>
                     </div>
                 </div>
+                <?php if ($walletObject) { ?>
+                    <div class="sectionWrp paymentOptions">
+                        <p class="title"><span class="check">2.</span> <span class="txt">Choose Amount</span> <span class="edit">edit</span></p>
+                        <div id="walletOption" style="display:none;">
+                            <form id="walletform" name="walletform">
+                                <?php
+                                foreach ($walletObject as $wallet) {
+                                    if ($wallet->type == '1') {
+                                        $walletname = 'Cash Wallet';
+                                        $fund = $wallet->fund;
+                                    }
+                                    if ($wallet->type == '2') {
+                                        $walletname = 'RP Wallet';
+                                        $fund = $wallet->fund * 75 / 100;
+                                    }
+                                    if ($wallet->type == '3') {
+                                        $walletname = 'Commision Wallet';
+                                        $fund = $wallet->fund;
+                                    }
+                                    ?>
+                                    <input type="checkbox" value="<?php echo $fund; ?>" name="wallet_type"><?php echo $walletname; ?>  
+                                <?php } ?>
+                                <br/><br/>
+                                <input type="button" value="Make Payment" onclick="walletamountcalculation();" class="btn-flat-green ui-btn-grey">   
+                            </form>
+                        </div>
+                    </div>
+<?php } ?>
 
                 <div class="sectionWrp paymentOptions">
-                    <p class="title"><span class="check">2.</span> <span class="txt">Choose Amount</span> <span class="edit">edit</span></p>
-                    <div id="walletOption" style="display:none;">
-                        <form id="walletform" name="walletform">
-                            <input type="checkbox" value="1" name="wallet_type[]">Cash Wallet 
-                            <input type="checkbox" value="2" name="wallet_type[]">RP Wallet
-                            <input type="checkbox" value="3" name="wallet_type[]">Commision Wallet
-                            <br/><br/>
-                            <input type="button" value="Make Payment" onclick="walletamountcalculation();" class="btn-flat-green ui-btn-grey">   
-                        </form>
-                            </div>
-                            </div>
+                    <p class="title"><span class="check">2.</span> <span class="txt">Make Payment</span> <span class="edit">edit</span></p>
+                    <div id="paymentOption" style="display:none;">
+                        <input type="radio" value="paypal" name="payment_mode">Paypal 
+                         <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" id="frmPayPal">
+                            <input type="hidden" name="business" value="pnirbhaylal@maverickinfosoft.com">
+                            <input type="hidden" name="cmd" value="_xclick">
+                            <input type="hidden" name="item_name" value="<?php echo $packageObject->name; ?>">
+                            <input type="hidden" name="item_number" value="1">
+                            <input type="hidden" name="credits" value="">
+                            <input type="hidden" name="userid" value="<?php Yii::app()->session['userid']; ?>">
+                            <input type="hidden" id ="ppamount" name="amount" value="<?php echo $packageObject->amount + Yii::app()->session['amount']; ?>">
+                            <input type="hidden" name="no_shipping" value="1">
+                            <input type="hidden" name="currency_code" value="USD">
+                            <input type="hidden" name="handling" value="0">
+                            <input type="hidden" name="cancel_return" value="">
+                            <input type="hidden" name="return" value="http://localhost/package/thankyou/">
 
-                            <div class="sectionWrp paymentOptions">
-                                <p class="title"><span class="check">2.</span> <span class="txt">Make Payment</span> <span class="edit">edit</span></p>
-                                <div id="paymentOption" style="display:none;">
-                                    <input type="radio" value="paypal" name="payment_mode">Paypal 
-                                    <input type="radio" value="rp" name="payment_mode">Use RP
-                                    <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" id="frmPayPal">
-                                        <input type="hidden" name="business" value="pnirbhaylal@maverickinfosoft.com">
-                                        <input type="hidden" name="cmd" value="_xclick">
-                                        <input type="hidden" name="item_name" value="<?php echo $packageObject->name; ?>">
-                                        <input type="hidden" name="item_number" value="1">
-                                        <input type="hidden" name="credits" value="">
-                                        <input type="hidden" name="userid" value="<?php Yii::app()->session['userid']; ?>">
-                                        <input type="hidden" name="amount" value="<?php echo $packageObject->amount + Yii::app()->session['amount']; ?>">
-                                        <input type="hidden" name="no_shipping" value="1">
-                                        <input type="hidden" name="currency_code" value="USD">
-                                        <input type="hidden" name="handling" value="0">
-                                        <input type="hidden" name="cancel_return" value="">
-                                        <input type="hidden" name="return" value="/transaction/">
-
-                                    </form> 
-                                    <input type="button" value="Make Payment" onclick="makepayment();" class="btn-flat-green ui-btn-grey">   
-                                </div>
-                            </div>
+                        </form> 
+                        <input type="button" value="Make Payment" onclick="makepayment();" class="btn-flat-green ui-btn-grey">   
                     </div>
                 </div>
             </div>
         </div>
-        <input type="hidden" id="totalAmount" value="<?php echo $packageObject->amount + Yii::app()->session['amount']; ?>">
-        <input type="hidden" id="coupon_discount_price" value=""> 
-        <script type="text/javascript">
+    </div>
+</div>
+<input type="hidden" id="totalAmount" value="<?php echo $packageObject->amount + Yii::app()->session['amount']; ?>">
+<input type="hidden" id="coupon_discount_price" value=""> 
+<input type="hidden" id="wallet" value="<?php echo (!empty($walletObject)) ? "1" : "0"; ?>">
+<script type="text/javascript">
 
-            function Couponapply() {
-                var coupon_code = $('#coupon_code').val();
-                if (coupon_code == '')
-                {
-                    document.getElementById("coupon_error").style.display = "block";
-                    document.getElementById("coupon_error").innerHTML = "Please enter a domain name.";
-                    document.getElementById("coupon_error").focus();
-                } else {
-                    var dataString = 'coupon_code=' + coupon_code;
-                    var url = $('#URL').val();
-                    $.ajax({
-                        type: "GET",
-                        url: "couponapply",
-                        data: dataString,
-                        cache: false,
-                        success: function (html) {
-                            if (html == 0)
-                            {
-                                document.getElementById("coupon_error").style.display = "block";
-                                document.getElementById("coupon_error").innerHTML = "Incorrect coupon code";
-                                $("#coupon_error").fadeOut(5000);
-                            } else {
-                                htmlTag = html.split("_");
-                                $('#coupon_code').val('');
-                                document.getElementById("coupon_success").style.display = "block";
-                                document.getElementById("coupon_success").innerHTML = "Coupon code applied";
-                                document.getElementById("totalpayable").innerHTML = htmlTag[0];
-                                document.getElementById("coupon_discount").style.display = "";
-                                document.getElementById("total-discount").innerHTML = htmlTag[1];
-                                document.getElementById("totalAmount").value = htmlTag[0];
-                                document.getElementById("coupon_discount_price").value = htmlTag[1];
-                                $("#coupon_success").fadeOut(5000);
-                            }
-                        }
-
-                    });//@ sourceURL=pen.js
-
-                }
-            }
-            function proceedPayment()
-            {
-                var coupon_discount = $('#coupon_discount_price').val();
-                var totalAmount = $('#totalAmount').val();
-                var dataString = 'datasave=yes&totalAmount=' + totalAmount + '&coupon_discount=' + coupon_discount;
-                $.ajax({
-                    type: "GET",
-                    url: "orderadd",
-                    data: dataString,
-                    cache: false,
-                    success: function (html) {
-                        if (html == 1)
-                        {
-                            $('#cartDiv').fadeOut();
-                            $('#editIcon').fadeIn();
-                            document.getElementById('walletOption').style.display = "block";
-
-                        }
+    function Couponapply() {
+        var coupon_code = $('#coupon_code').val();
+        if (coupon_code == '')
+        {
+            document.getElementById("coupon_error").style.display = "block";
+            document.getElementById("coupon_error").innerHTML = "Please enter a domain name.";
+            document.getElementById("coupon_error").focus();
+        } else {
+            var dataString = 'coupon_code=' + coupon_code;
+            var url = $('#URL').val();
+            $.ajax({
+                type: "GET",
+                url: "couponapply",
+                data: dataString,
+                cache: false,
+                success: function (html) {
+                    if (html == 0)
+                    {
+                        document.getElementById("coupon_error").style.display = "block";
+                        document.getElementById("coupon_error").innerHTML = "Incorrect coupon code";
+                        $("#coupon_error").fadeOut(5000);
+                    } else {
+                        htmlTag = html.split("_");
+                        $('#coupon_code').val('');
+                        document.getElementById("coupon_success").style.display = "block";
+                        document.getElementById("coupon_success").innerHTML = "Coupon code applied";
+                        document.getElementById("totalpayable").innerHTML = htmlTag[0];
+                        document.getElementById("coupon_discount").style.display = "";
+                        document.getElementById("total-discount").innerHTML = htmlTag[1];
+                        document.getElementById("totalAmount").value = htmlTag[0];
+                        document.getElementById("coupon_discount_price").value = htmlTag[1];
+                        $("#coupon_success").fadeOut(5000);
                     }
-                });
-            }
-
-            function OpenDiv()
-            {
-                $('#cartDiv').fadeIn();
-                $('#paymentOption').fadeOut();
-            }
-            function makepayment()
-            {
-                var valx = $('input[name=payment_mode]:checked').val();
-                if (valx == 'paypal')
-                {
-                    document.getElementById("frmPayPal").submit();
                 }
-                if (valx == 'rp')
+
+            });//@ sourceURL=pen.js
+
+        }
+    }
+    function proceedPayment()
+    {
+        var coupon_discount = $('#coupon_discount_price').val();
+        var walletVal = $('#wallet').val();
+        var totalAmount = $('#totalAmount').val();
+        var dataString = 'datasave=yes&totalAmount=' + totalAmount + '&coupon_discount=' + coupon_discount;
+        $.ajax({
+            type: "GET",
+            url: "orderadd",
+            data: dataString,
+            cache: false,
+            success: function (html) {
+                if (html == 1)
                 {
-                    location.href = "/transaction/thankyou/";
-                }
-            }
-            function walletamountcalculation()
-            {
-                var form = document.walletform;
-                var totalAmount = $('#totalAmount').val();
-                var dataString = $(form).serialize()+'&totalAmount='+totalAmount;
-
-
-                $.ajax({
-                    type: 'POST',
-                    url: '/package/walletcalculation/',
-                    data: dataString,
-                    success: function (data) { alert(data);return false;
-                        $('#myResponse').html(data);
-
-
+                    $('#cartDiv').fadeOut();
+                    $('#editIcon').fadeIn();
+                    if (walletVal == 1)
+                    {
+                        document.getElementById('walletOption').style.display = "block";
+                    } else {
+                        document.getElementById('paymentOption').style.display = "block";
                     }
-                });
-                return false;
+
+                }
             }
-        </script>    
+        });
+    }
+
+    function OpenDiv()
+    {
+        $('#cartDiv').fadeIn();
+        $('#paymentOption').fadeOut();
+    }
+    function makepayment()
+    {
+        var valx = $('input[name=payment_mode]:checked').val();
+        if (valx == 'paypal')
+        {
+            document.getElementById("frmPayPal").submit();
+        }
+        
+    }
+    function walletamountcalculation()
+    {
+        var input = document.getElementsByName("wallet_type");
+        var totalAmount = $('#totalAmount').val();
+        var total = 0;
+        for (var i = 0; i < input.length; i++) {
+            if (input[i].checked) {
+                total += parseFloat(input[i].value);
+            }
+        }
+        var payableAmount = totalAmount - total;
+        
+        $('#totalAmount').val(payableAmount);
+        $('#ppamount').val(payableAmount); 
+        document.getElementById('walletOption').style.display = "none";
+        document.getElementById('paymentOption').style.display = "block";
+     }
+</script>    
