8cb70088c98442ce2fda42773d37aa704d933fd1
ram <ramhemareddy@mgmail.com>
2015-06-03 14:02:05 +0530

    Added Image cropper

--- protected/components/BaseClass.php
+++ protected/components/BaseClass.php
@@ -56,10 +56,6 @@ class BaseClass extends Controller {
             return $walletObject; 
                         
     }
-<<<<<<< HEAD
-    public static function getUnredMails($userId){
-        return Mail::model()->count(array('condition'=>'from_user_id='.$userId. ' AND type = 0'));
-=======
     public static function transactionStatus() {
         $userId = Yii::app()->session['userid'];
         $transactionObject = Transaction::model()->findByAttributes(array('user_id'=>$userId));
@@ -68,8 +64,7 @@ class BaseClass extends Controller {
                         
     }
     public static function getUnredMails($userId){
-        return Mail::model()->count(array('condition'=>'from_user_id='.$userId. ' AND status = 0'));
->>>>>>> 78031dbd0bbe159f66c59b17030d152370d81fb6
+        return Mail::model()->count(array('condition'=>'from_user_id='.$userId. ' AND type = 0'));
     }
 
     public static function isAdmin() {
