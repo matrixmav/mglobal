83f623ae571101e475a1e15ab95872782ee4b40f
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-06-03 14:54:14 +0530

    update

--- protected/models/Transaction.php
+++ protected/models/Transaction.php
@@ -144,7 +144,6 @@ class Transaction extends CActiveRecord
             }
             $createdTime = new CDbExpression('NOW()');
             $tarnsactionID = BaseClass::gettransactionID();
-            Yii::app()->session['$ptarnsactionID'] = $tarnsactionID;
             $transactionObjuser = new Transaction;
             $transactionObjuser->user_id = $userObject->id;
             $transactionObjuser->transaction_id = $tarnsactionID;
