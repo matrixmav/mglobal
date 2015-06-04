bdc78b2113e218d02e01675184f076d84ff1d712
surya.asati <surya.asati@MGIS-PC0031.MGIS.COM>
2015-06-04 17:45:31 +0530

    Update database

--- sql/alter.sql
+++ sql/alter.sql
@@ -13,3 +13,5 @@ ALTER TABLE `build_temp_header` ADD `menu` TEXT NOT NULL AFTER `header_content`;
 
 ALTER TABLE `user_has_template` ADD `user_menu` TEXT NOT NULL AFTER `user_id`;
 
+
+ALTER TABLE `user_has_template` ADD `logo_height` VARCHAR(11) NOT NULL AFTER `custom_js`, ADD `logo_width` VARCHAR(11) NOT NULL AFTER `logo_height`, ADD `contact_form` TEXT NOT NULL AFTER `logo_width`;
