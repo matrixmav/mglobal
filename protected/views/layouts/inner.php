45836341d26bc478b0e89f3debbcdffaa921aaf1
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-06-09 16:46:32 +0530

    updated

--- protected/views/layouts/inner.php
+++ protected/views/layouts/inner.php
@@ -751,6 +751,7 @@ License: You must have a valid license purchased only from themeforest(the above
             <!-- BEGIN CONTENT -->
             <div class="page-content-wrapper">
                 <div class="page-content">
+                    <span class="home-link" style="font-size:14px;float:right;"><?php echo date('Y-m-d H:i:s', strtotime('now'))."\n";?> | <?php echo $userObject->created_at;?></span>
                     <!-- BEGIN SAMPLE PORTLET CONFIGURATION MODAL FORM-->
                     <!-- /.modal -->
                     <!-- END SAMPLE PORTLET CONFIGURATION MODAL FORM-->
