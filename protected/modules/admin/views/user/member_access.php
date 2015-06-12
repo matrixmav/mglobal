38e9c9e3179bca0c4a96708742ecaebb34836107
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-06-12 16:55:13 +0530

    Merge branch 'master' of https://github.com/matrixmav/mglobal
    Conflicts:
    	protected/controllers/UserController.php

--- protected/modules/admin/views/user/member_access.php
+++ protected/modules/admin/views/user/member_access.php
@@ -4,10 +4,22 @@ $this->breadcrumbs = array(
     'Member Access'
 );
 ?>
+<?php 
+if(!empty($error)){
+    echo "<p class='error'>".$error."</p>";
+}
+
+?>
+<?php 
+if(!empty($error)){
+    echo "<p class='success'>".$success."</p>";
+}
+
+?>
 <form class="form-horizontal" role="form" id="form_admin_reservation" enctype="multipart/form-data" action="/admin/userhasaccess/memberaccess" method="post" onsubmit="return validateForm()">
 <div class="col-md-12 form-group">
     <div class="col-md-6">
-    <select name="to_email" id="to_email"  class="form-control">
+    <select name="admin_id" id="admin_id"  class="form-control">
             <option value="">Select Member</option>
             <?php foreach($emailObject as $email){
               if(!empty($mailObject))
@@ -24,13 +36,13 @@ $this->breadcrumbs = array(
 </div>
 <div class="col-md-12 form-group">
     <div class="col-md-6">
-        <input type="checkbox" name="access[]" id="builder">Builder<br/>
-        <input type="checkbox" name="access[]" id="ads">Ads<br/>
-        <input type="checkbox" name="access[]" id="package">package<br/>
-        <input type="checkbox" name="access[]" id="reports">Reports<br/>
-        <input type="checkbox" name="access[]" id="user">Member Management<br/>
-        <input type="checkbox" name="access[]" id="mail">Mail<br/>
-        <input type="checkbox" name="access[]" id="memberaccess">Member Access<br/>
+        <input type="checkbox" name="access[]" value="builder">Builder<br/>
+        <input type="checkbox" name="access[]" value="ads">Ads<br/>
+        <input type="checkbox" name="access[]" value="package">package<br/>
+        <input type="checkbox" name="access[]" value="reports">Reports<br/>
+        <input type="checkbox" name="access[]" value="user">Member Management<br/>
+        <input type="checkbox" name="access[]" value="mail">Mail<br/>
+        <input type="checkbox" name="access[]" value="memberaccess">Member Access<br/>
     </div>
 </div>
  
