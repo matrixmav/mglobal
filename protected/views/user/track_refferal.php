07dcb2646f75453e47d288bd24287e533fd1c4a6
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-06-12 15:18:37 +0530

    Merge branch 'master' of https://github.com/matrixmav/mglobal
    Conflicts:
    	protected/controllers/UserController.php

--- protected/views/user/track_refferal.php
+++ protected/views/user/track_refferal.php
@@ -2,11 +2,9 @@
 /* @var $this OrderController */
 /* @var $dataProvider CActiveDataProvider */
 
-$this->breadcrumbs = array(
-    'Summary' => array('summary/checkinvestment'),
-    'Check Investment',
+$this->breadcrumbs=array(
+	'Orders List',
 );
- 
 
 $this->menu=array(
 	array('label'=>'Create Order', 'url'=>array('create')),
@@ -14,47 +12,19 @@ $this->menu=array(
 );
  
 ?>
-<style>
-    .confirmBtn{left: 333px;
-    position: absolute;
-    top: 0;}
-    
-    .confirmOk{left: 610px;
-    position: absolute;
-    top: 8px;}
-    .confirmMenu{position: relative;}
-</style>
-<div class="col-md-12">
-    
-        <div class="expiration margin-topDefault confirmMenu">
-                    
-    <form id="regervation_filter_frm" name="regervation_filter_frm" method="post" action="/transaction/list">
-    <div class="input-group input-large date-picker input-daterange">
-        <input type="text" name="from" placeholder="To Date" class="datepicker form-control">
-        <span class="input-group-addon">
-        to </span>
-        <input type="text" name="to" data-provide="datepicker" placeholder="From Date" class="datepicker form-control">
-    </div>
-    <?php 
-    /*$statusId =   1;
-    if(isset($_REQUEST['res_filter'])){
-      $statusId =   $_REQUEST['res_filter'];
-    } ?>
-    
-    <select class="customeSelect howDidYou form-control input-medium select2me confirmBtn" id="ui-id-5" name="res_filter">
-                <option value="1" <?php if($statusId == 1){ echo "selected"; } ?> >Active</option>
-                <option value="0" <?php if($statusId == 3){ echo "selected"; } ?> >In Active</option>
-            </select>
-    </div>*/?>
-        <input type="submit" class="btn btn-primary confirmOk" value="OK" name="submit" id="submit" style="left:340px!important;top:0px!important;"/>
-    </form>
+    <?php //echo "<pre>"; print_r($orderObject);?>
+       <div class="main">
+      <div class="container">
 
-</div>
-<div class="row">
-    <div class="col-md-12">
+        <!-- BEGIN SIDEBAR & CONTENT -->
+        <div class="row margin-bottom-40">
+         <!-- BEGIN SIDEBAR -->
+          <!-- END SIDEBAR -->
+          <!-- BEGIN CONTENT -->
+          <div class="col-md-10 col-sm-9">
        
         <?php 
-        
+          
         $this->widget('zii.widgets.grid.CGridView', array(
 	'id'=>'city-grid',
 	'dataProvider'=>$dataProvider,
@@ -72,6 +42,7 @@ $this->menu=array(
 	),	
 	'columns'=>array(
 		//'idJob',
+            
                 array(
                     'name'=>'id',
                     'header'=>'<span style="white-space: nowrap;">Sl. No &nbsp; &nbsp; &nbsp;</span>',
@@ -80,41 +51,29 @@ $this->menu=array(
 		array(
                     'name'=>'full_name',
                     'header'=>'<span style="white-space: nowrap;">Name &nbsp; &nbsp; &nbsp;</span>',
-                    'value'=>'isset($data->full_name)? ucwords($data->full_name):""',
-		),
-            array(
-                    'name'=>'id',
-                    'header'=>'<span style="white-space: nowrap;">Purchased Package &nbsp; &nbsp; &nbsp;</span>',
-                    'value'=>'isset($data->package->name)? ucwords($data->package->name):""',
+                    'value'=>'isset($data->full_name)?$data->package->full_name:""',
 		),
                 array(
-                    'name'=>'transaction_id',
-                    'header'=>'<span style="white-space: nowrap;">Paid Amount &nbsp; &nbsp; &nbsp;</span>',
-                    'value'=>'isset($data->package->amount)? number_format($data->package->amount,2):""',
+                    'name'=>'id',
+                    'header'=>'<span style="white-space: nowrap;">Refered In &nbsp; &nbsp; &nbsp;</span>',
+                    'value'=>'($data->social==fb) ? facebook ($data->social==tw) ? twitter : email',
 		),
-            array(
+               array(
                     'name'=>'created_at',
-                    'header'=>'<span style="white-space: nowrap;">Order Date &nbsp; &nbsp; &nbsp;</span>',
-                    'value'=>'$data->orderSummary->created_at',
+                    'header'=>'<span style="white-space: nowrap;">Created At &nbsp; &nbsp; &nbsp;</span>',
+                    'value'=>'$data->created_at',
 		),
-            array(
-                    'name'=>'status',
-                    'header'=>'<span style="white-space: nowrap;">Transfer Status &nbsp; &nbsp; &nbsp;</span>',
-                    'value'=>'($data->orderSummary->status == 1) ? "Completed" : "Pending"',
+                
+		array(
+			'name'=>'id',
+                        'header'=>'<span style="white-space: nowrap;">Position &nbsp; &nbsp; &nbsp;</span>',
+			'value'=>'$data->position',
 		),
-            
              
 		 
-	),
+	 ),
 )); ?>
                     
 
+      </div>
     </div>
-</div>
-<script>
-    $(function () {
-                $('.datepicker').datepicker({
-                    format: 'yyyy-mm-dd'
-                });
-            });
-    </script>
