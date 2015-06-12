be15ca1fe2b1936744106508e0847b7cbc1d12db
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-06-12 19:32:36 +0530

    Merge branch 'master' of https://github.com/matrixmav/mglobal
    Conflicts:
    	protected/controllers/UserController.php

--- protected/modules/admin/views/user/member_access.php
+++ protected/modules/admin/views/user/member_access.php
@@ -30,20 +30,32 @@ if(!empty($success)){
     <div class="col-md-6">
         <input type="checkbox" name="access[]" value="builder" <?php if(in_array('builder',$accessArr)){?> checked="checked" <?php }?>>Builder<br/>
         <input type="checkbox" name="access[]" value="ads" <?php if(in_array('ads',$accessArr)){?> checked="checked" <?php }?>>Ads<br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="access[]" value="ads_add" <?php if(in_array('ads_add',$accessArr)){?> checked="checked" <?php }?>>Ads Add<br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="access[]" value="ads_list" <?php if(in_array('ads_list',$accessArr)){?> checked="checked" <?php }?>>Ads List<br/>
+        <br/>
         <input type="checkbox" name="access[]" value="package" <?php if(in_array('package',$accessArr)){?> checked="checked" <?php }?>>package<br/>
+         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="access[]" value="package_list" <?php if(in_array('package_list',$accessArr)){?> checked="checked" <?php }?>>Package List<br/>
+         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="access[]" value="package_add" <?php if(in_array('package_add',$accessArr)){?> checked="checked" <?php }?>>Package Add<br/>
+         <br/>
         <input type="checkbox" name="access[]" value="reports" <?php if(in_array('reports',$accessArr)){?> checked="checked" <?php }?>>Reports<br/>
-        <input type="checkbox" name="access[]" value="user" <?php if(in_array('user',$accessArr)){?> checked="checked" <?php }?>>Member Management<br/>
-        <input type="checkbox" name="access[]" value="mail" <?php if(in_array('mail',$accessArr)){?> checked="checked" <?php }?>>Mail<br/>
-        <input type="checkbox" name="access[]" value="memberaccess" <?php if(in_array('memberaccess',$accessArr)){?> checked="checked" <?php }?>>Member Access<br/>
-        <input type="checkbox" name="access[]" value="wallet" <?php if(in_array('wallet',$accessArr)){?> checked="checked" <?php }?>>Wallet<br/>
-        <input type="checkbox" name="access[]" value="geneology" <?php if(in_array('geneology',$accessArr)){?> checked="checked" <?php }?>>Geneology<br/>
-       <input type="checkbox" name="access[]" value="document" <?php if(in_array('document',$accessArr)){?> checked="checked" <?php }?>>document<br/>
-       <input type="checkbox" name="access[]" value="testimonial" <?php if(in_array('testimonial',$accessArr)){?> checked="checked" <?php }?>>Testimonial<br/>
-       <input type="checkbox" name="access[]" value="package_list" <?php if(in_array('package_list',$accessArr)){?> checked="checked" <?php }?>>Package List<br/>
-       <input type="checkbox" name="access[]" value="package_add" <?php if(in_array('package_add',$accessArr)){?> checked="checked" <?php }?>>Package Add<br/>
-       <input type="checkbox" name="access[]" value="ads_add" <?php if(in_array('ads_add',$accessArr)){?> checked="checked" <?php }?>>Ads Add<br/>
-       <input type="checkbox" name="access[]" value="ads_list" <?php if(in_array('ads_list',$accessArr)){?> checked="checked" <?php }?>>Ads List<br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="access[]" value="userreport" <?php if(in_array('userreport',$accessArr)){?> checked="checked" <?php }?>>User Report<br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="access[]" value="reportaddress" <?php if(in_array('reportaddress',$accessArr)){?> checked="checked" <?php }?>>Address Report<br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="access[]" value="reportsponsor" <?php if(in_array('reportsponsor',$accessArr)){?> checked="checked" <?php }?>>Sponsor Report<br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="access[]" value="reportpackage" <?php if(in_array('reportpackage',$accessArr)){?> checked="checked" <?php }?>>Package Report<br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="access[]" value="reporttransaction" <?php if(in_array('reporttransaction',$accessArr)){?> checked="checked" <?php }?>>Transaction Report<br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="access[]" value="reportsocial" <?php if(in_array('reportsocial',$accessArr)){?> checked="checked" <?php }?>>Social Profile Report<br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="access[]" value="reportcontact" <?php if(in_array('reportcontact',$accessArr)){?> checked="checked" <?php }?>>Contact Report<br/>
+        <br/>
+       <input type="checkbox" name="access[]" value="mail" <?php if(in_array('mail',$accessArr)){?> checked="checked" <?php }?>>Mail<br/>
+       <input type="checkbox" name="access[]" value="user" <?php if(in_array('user',$accessArr)){?> checked="checked" <?php }?>>Operation<br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="access[]" value="wallet" <?php if(in_array('usermg',$accessArr)){?> checked="checked" <?php }?>>Member Management<br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="access[]" value="wallet" <?php if(in_array('wallet',$accessArr)){?> checked="checked" <?php }?>>Wallet<br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="access[]" value="geneology" <?php if(in_array('geneology',$accessArr)){?> checked="checked" <?php }?>>Geneology<br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="access[]" value="document" <?php if(in_array('document',$accessArr)){?> checked="checked" <?php }?>>document<br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="access[]" value="testimonial" <?php if(in_array('testimonial',$accessArr)){?> checked="checked" <?php }?>>Testimonial<br/>
        
+       <input type="checkbox" name="access[]" value="memberaccess" <?php if(in_array('memberaccess',$accessArr)){?> checked="checked" <?php }?>>Member Access<br/>
+        
     </div>
 </div>
  
