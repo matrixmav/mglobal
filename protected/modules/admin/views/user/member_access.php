8946d27129f543f5d3d2182e7905aef48e7adc5e
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-06-12 16:11:41 +0530

    Merge branch 'master' of https://github.com/matrixmav/mglobal
    Conflicts:
    	protected/controllers/UserController.php

--- protected/modules/admin/views/user/member_access.php
+++ protected/modules/admin/views/user/member_access.php
@@ -4,3 +4,23 @@ $this->breadcrumbs = array(
     'Member Access'
 );
 ?>
+<form class="form-horizontal" role="form" id="form_admin_reservation" enctype="multipart/form-data" action="/admin/userhasaccess/memberaccess" method="post" onsubmit="return validateForm()">
+
+<div class="col-md-12 form-group">
+    <div class="col-md-6">
+        <input type="checkbox" name="access[]" id="builder">Builder
+        <input type="checkbox" name="access[]" id="ads">Ads
+        <input type="checkbox" name="access[]" id="package">package
+        <input type="checkbox" name="access[]" id="reports">Reports
+        <input type="checkbox" name="access[]" id="users">Member Management
+        <input type="checkbox" name="access[]" id="mail">Mail
+    </div>
+</div>
+ 
+<div class="col-md-12 form-group">
+    <label class="col-md-2"></label>
+    <div class="col-md-6">
+        <input type="submit" class="btn green" name="submit" id="submit" size="60" maxlength="75" class="textBox" value="Submit" />
+    </div>
+</div> 
+</form>
