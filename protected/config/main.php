4507c3f148a29222b117cde7c128a686c2b9610b
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-06-12 12:21:37 +0530

    Merge branch 'master' of https://github.com/matrixmav/mglobal
    Conflicts:
    	protected/controllers/UserController.php

--- protected/config/main.php
+++ protected/config/main.php
@@ -113,7 +113,7 @@ return array(
 				'connectionString' => 'mysql:host=localhost;dbname=mglobal',
 				'emulatePrepare' => true,
 				'username' => 'root',
-				'password' => 'root',
+				'password' => '',
 				'charset' => 'utf8',
                                 'tablePrefix' => 'tbl_',
 		),
