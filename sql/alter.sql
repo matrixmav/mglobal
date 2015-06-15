e7bf081e75739f6bbd2c9fe821b98ef265938cd8
nidhi <nidhisati@maverickinfosoft.com>
2015-06-15 09:39:09 +0530

    Merge branch 'master' of https://github.com/matrixmav/mglobal
    Conflicts:
    	protected/controllers/UserController.php

--- sql/alter.sql
+++ sql/alter.sql
@@ -65,3 +65,51 @@ INSERT INTO `mglobal`.`user` (`id`, `sponsor_id`, `name`, `password`, `position`
 INSERT INTO `mglobal`.`user` (`id`, `sponsor_id`, `name`, `password`, `position`, `user_sponsor_id`, `full_name`, `email`, `country_id`, `country_code`, `phone`, `date_of_birth`, `skype_id`, `facebook_id`, `twitter_id`, `master_pin`, `unique_id`, `status`, `activation_key`, `forget_key`, `forget_status`, `role_id`, `created_at`, `updated_at`) VALUES (NULL, '', 'sales', '', NULL, '', '', 'sales@mglobally.com', '', '', '', '', NULL, NULL, NULL, '', '', '1', NULL, NULL, NULL, '2', '2015-06-05', CURRENT_TIMESTAMP), (NULL, '', 'marketing@mglobally.com', '', NULL, '', '', '', '', '', '', '', NULL, NULL, NULL, '', '', '1', NULL, NULL, NULL, '2', '2015-06-01', CURRENT_TIMESTAMP);
 
 ALTER TABLE `user_pages` ADD `status` INT(11) NOT NULL DEFAULT '1' AFTER `page_form`;
+
+
+
+/*12-06-2015*/
+
+--
+-- Table structure for table `user_has_access`
+--
+
+CREATE TABLE IF NOT EXISTS `user_has_access` (
+`id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `access` varchar(255) NOT NULL,
+  `created_at` date NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `user_has_access`
+--
+
+INSERT INTO `user_has_access` (`id`, `user_id`, `access`, `created_at`) VALUES
+(1, 22, 'ads,package,reports,user', '2015-06-12'),
+(2, 23, 'builder,ads,package,reports', '2015-06-12'),
+(3, 21, 'ads_list,package_list,reports,userreport,reportaddress,reportcontact,mail,user,wallet', '2015-06-12'),
+(4, 1, 'reports,user,mail,memberaccess,package,ads,builder,ads_add,ads_list,package_list,package_add,userreport,reportaddress,reportsponsor,reportpackage,reporttransaction,reportsocial,reportcontact,usermg,wallet,geneology,document,testimonial', '2015-06-12');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_has_access`
+--
+ALTER TABLE `user_has_access`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_has_access`
+--
+ALTER TABLE `user_has_access`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
