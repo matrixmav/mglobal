091f7fbe7b2c5841290ec4e7827c74a7c5072e6f
nidhi <nidhisati@maverickinfosoft.com>
2015-06-15 11:24:18 +0530

    Merge branch 'master' of https://github.com/matrixmav/mglobal
    Conflicts:
    	protected/controllers/UserController.php

--- protected/views/layouts/inner.php
+++ protected/views/layouts/inner.php
@@ -135,7 +135,7 @@ License: You must have a valid license purchased only from themeforest(the above
         <script src="<?php echo Yii::app()->baseUrl . '/ckeditor/ckeditor.js'; ?>"></script>
         <script type="text/javascript" src="<?php echo Yii::app()->baseUrl . '/ckfinder/ckfinder.js'; ?>"></script>
         <script src="/metronic/assets/scripts/core/app.js"></script>
-        <script type="text/javascript" src="/chat/js/chat.js"></script>
+        <!--<script type="text/javascript" src="/chat/js/chat.js"></script>-->
         <script type="text/javascript">
             jQuery(document).ready(function () {
                 App.init();
@@ -159,11 +159,11 @@ License: You must have a valid license purchased only from themeforest(the above
                 });
             });
              
-           function openChat()
+           /*function openChat()
            {
              var IDSVal = document.getElementById('username').value;
              chatWith(IDSVal);  
-           }
+           }*/
         </script>
 
 
