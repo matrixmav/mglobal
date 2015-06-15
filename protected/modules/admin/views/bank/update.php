934313f7804416bf7220e8a11f78b7b756bbf393
ram <ramhemareddy@mgmail.com>
2015-06-15 15:17:36 +0530

    Removed unwanted files and folders

--- protected/modules/admin/views/bank/update.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* @var $this BankController */
-/* @var $model Bank */
-
-$this->breadcrumbs=array(
-	'Banks'=>array('index'),
-	'Update',
-);
-$this->renderPartial('_form', array('model'=>$model)); ?>
