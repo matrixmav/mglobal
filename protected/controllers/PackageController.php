b21a8c7c57175b56e0d4a883fe8dd988e3b0bd8c
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-06-17 12:57:29 +0530

    update

--- protected/controllers/PackageController.php
+++ protected/controllers/PackageController.php
@@ -77,9 +77,8 @@ class PackageController extends Controller {
         $tarnsactionId = BaseClass::gettransactionID();
         if(!empty($_POST) && $_POST['transactionId']){
             $tarnsactionId = $_POST['transactionId'];
-            Yii::app()->session['transactionId'] = $tarnsactionId;
         }
-        Yii::app()->session['transactionId'] = $tarnsactionId;
+        
         $transactionObject = Transaction::model()->find(array('condition' => 'user_id =' . Yii::app()->session['userid'] . ' AND transaction_id = ' . $tarnsactionId));
 
         $total = $_POST['totalAmount'] - $_POST['couponDiscount'];
@@ -205,6 +204,7 @@ class PackageController extends Controller {
             </div>
             </div>';
         $SuggestedDomain = "";
+        unset(Yii::app()->session['domain']);
         $userEnteredDomain = Yii::app()->session['domain'];
         if ($userEnteredDomain != '') {
             $doaminArr = explode('.', $userEnteredDomain);
