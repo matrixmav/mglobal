37da5604c9aaade28bf5e90523be3ddfcd889e72
nidhi <nidhisati@maverickinfosoft.com>
2015-06-18 15:10:02 +0530

    update

--- protected/views/moneyTransfer/transfer.php
+++ protected/views/moneyTransfer/transfer.php
@@ -69,3 +69,52 @@ $this->breadcrumbs = array(
 <style>
         #s2id_search_username{ width: 100% !important;}
 </style>
+<script>
+    function validationfrom()
+{
+    $('#transaction_error').html("");
+    if($('#transactiontype').val()=='')
+    {
+       $('#transaction_error').html("Please choose wallet type"); 
+       return false;
+    }
+    $('#search_user_error').html("");
+   if($('#search_username').val()=='')
+    {
+       $('#search_user_error').html("Please choose user to transfer amount."); 
+       return false;
+    }
+    $('#amount_error').html("");
+     if($('#transaction_data_amt').val()== '0.00')
+    {
+       $('#amount_error').html("Transfer amount can not be 0.00"); 
+       return false;
+    }
+    if($('#paid_amount').val()== '')
+    {
+       $('#email_error').html("Transfer amount can not be blank"); 
+       return false;
+    }
+    var fund = $('#transaction_data_amt').val();
+    var fundFinal = Number(fund.replace(/[^0-9\.]+/g,""))
+    var fundVal = parseFloat($('#paid_amount').val());
+    var totalPaid = fundVal + fundVal*1/100;
+    $('#email_error').html("");   
+    if(fundFinal < totalPaid)
+    {
+       $('#email_error').html("You don't have sufficient amount to transfer. Please reduce amount"); 
+       return false;
+    }
+    
+    $('#email_error').html("");   
+    if(fundFinal < fundVal)
+    {
+       $('#email_error').html("Transfer amount can not be more than existing amount."); 
+       return false;
+    }
+    
+    
+ }
+ 
+
+    </script>
