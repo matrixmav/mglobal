e592e9387957c3b123a9424701a1493672d0b470
nidhi <nidhisati@maverickinfosoft.com>
2015-06-18 09:32:32 +0530

    update

--- protected/controllers/OrderController.php
+++ protected/controllers/OrderController.php
@@ -335,12 +335,13 @@ class OrderController extends Controller {
     }
     
       protected function GetInvoiceButtonTitle($data, $row) {
+         $id = $data->id;
          $userId = Yii::app()->session['userid'];
         $userhasObject = UserHasTemplate::model()->find(array('condition' => 'order_id=' . $data['id']));
         $orderObject = Order::model()->find(array('condition' => 'id=' . $data['id']));
         if($orderObject->status==1)
         {
-        $title = '<a href="/order/invoice?id="'.$data->id.' title="Visit Website" target="_blank" class="btn red fa fa-edit margin-right15">Invoice</a>';
+        $title = '<a href="/order/invoice?id='.$id.'" title="Visit Website" target="_blank" class="btn red fa fa-edit margin-right15">Invoice</a>';
         }else{
         $title = '<a class="btn red fa fa-edit margin-right15" href="#">N/A</a>';
          }
