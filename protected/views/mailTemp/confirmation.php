55869dc6d783370657ddc53fa69f71778d84bea0
nidhi <nidhisati@maverickinfosoft.com>
2015-06-18 16:27:20 +0530

    update

--- protected/views/mailTemp/confirmation.php
+++ protected/views/mailTemp/confirmation.php
@@ -1,11 +1,58 @@
-<?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-'Hi,' . $model->full_name . '<br/> Congratulations! You have been registered successfully ' .
-                        '<strong>Please click the link below to activate your account:</strong><br/>' .
-                        '<a href="'.Yii::app()->getBaseUrl(true).'/user/confirm?activation_key=' . $rand . '">Click to activate </a>';
+<tr>
+    	   <td valign="" bgcolor="#efed6a" height="55" align="left" style="line-height:0px; font-size:16px">
+        	<table width="100%" cellspacing="0" cellpadding="0" border="0">
+          <tbody>
+            <tr>
+            	<td width="5%" valign="middle" align="center" style="line-height:22px; color: #828282; font-size:16px; font-family:'Nunito'"> </td>
+                <td width="90%" valign="middle" align="left" style="line-height:22px; color: #828282; font-size:16px; font-family:'Nunito'"> Hello Sandeep</td>
+                <td width="5%" valign="middle" align="center" style="line-height:22px; color: #828282; font-size:16px; font-family:'Nunito'"> </td>
+            </tr>
+            </tbody>
+            </table>
+        </td>
+    </tr>
+    
+    <!-- text description -->
+    <tr>
+    	   <td valign="" bgcolor="#fafafa" height="" align="left" style="line-height:0px; font-size:16px">
+        	<table width="100%" cellspacing="0" cellpadding="0" border="0">
+          <tbody>
+            <tr>
+            	<td width="5%" valign="middle" align="center" style="line-height:22px; color: #828282; font-size:16px; font-family:'Nunito'"> </td>
+                <td width="90%" valign="middle" align="left" style="line-height:22px; color: #828282; font-size:16px; font-family:'Nunito'"> 
+                <table width="100%" cellspacing="0" cellpadding="0" border="0">
+          <tbody>
+            <tr>
+            	  <td height="20" bgcolor="" style=""></td>
+            </tr>
+             <tr>
+            	  <td height=""  valign="middle" align="left" style="line-height:22px; color: #3fb90f; font-size:16px; font-family:'Nunito'">
+                  Thank you for signing up with us
+                  </td>
+                  <tr>
+            	  <td height="20" bgcolor="" style=""></td>
+            </tr>
+            <tr>
+            	  <td height=""  valign="middle" align="left" style="line-height:22px; color: #6b6b6b; font-size:16px; font-family:'Nunito'">
+                  Your email address <a href="" style="color:#f15c2b; display:inline">xyz@gmail.com</a>, has been set as the Registrant contact on <p style="color:#f15c2b; display:inline">mglobally </p>.com for UserName :<strong> xyz </strong> . To verify your email address, please click on the following link.
+                  </td>
+                  <tr>
+            	  <td height="20" bgcolor="" style=""></td>
+            </tr>
+            <tr>
+            	 <td height=""  valign="middle" align="left" style="line-height:22px; color: #3fb90f; font-size:16px; font-family:'Nunito'">
+                 <a href="" style="color:#3fb90f; display:inline">https://www.xyz.com/act-un/xyz</a>
+                  </td>
+            </tr>
+            <tr>
+            	  <td height="20" bgcolor="" style=""></td>
+            </tr>
+            <tr>
+            	  <td height=""  valign="middle" align="left" style="line-height:22px; color: #6b6b6b; font-size:16px; font-family:'Nunito'">
+                  Please note that failure to verify the Registrant contact email address will lead to deactivation of the respective user, if not completed within 3 days from the date of that action. Once deactivated, the member user will not function until the email address is verified.
+                  </td>
+                  </tr>
+                  <tr>
+            	  <td height="20" bgcolor="" style=""></td>
+            </tr>
--- protected/views/mailTemp/header.php
+++ protected/views/mailTemp/header.php
@@ -1,8 +1,38 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link href='http://fonts.googleapis.com/css?family=Nunito:400,300,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<table align="center" width="600" cellspacing="0" cellpadding="0" border="0">
+  <tbody>
+    <tr>
+      <td height="20"  style=""></td>
+    </tr>
+    <tr>
+      <td height="10" bgcolor="#f15c2b" style=""></td>
+    </tr>
+    <tr>
+      <td valign="" bgcolor="#fafafa" height="80" style="line-height:0px; border-bottom:1px solid #dfdfdf">
+      <table width="100%" cellspacing="0" cellpadding="0" border="0">
+          <tbody>
+            <tr>
+              <td width="5%" valign="middle" style="line-height:0px"></td>
+              <td width="40%" valign="middle" style="line-height:0px"><a target="_blank" href=""> <img width="" border="0" src="logo.png"> </a></td>
+              <td width="55%" valign="middle" style="line-height:0px; color:#f15c2b; font-family:'Nunito';">
+              <table width="100%" cellspacing="0" cellpadding="0" border="0">
+                  <tbody>
+                  	<tr>
+                    <td width="80%" valign="middle" align="right" height="20" style=" color:#f15c2b;"><img width="" border="0" src="livechat.png"> &nbsp;</td>
+                    </tr>
+                    <tr>
+                 <td width="100%" valign="middle" align="right" height="20" style=" color:#828282;font-size:14px; line-height:19px; font-family:'Nunito'"><strong> Customer Support:</strong> 1800 909 302 &nbsp; </td>
+                    </tr>
+                  </tbody>
+                </table></td>
+            </tr>
+          </tbody>
+        </table></td>
+    </tr>
