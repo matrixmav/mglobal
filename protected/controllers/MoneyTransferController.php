6ee6ca1f02b26bf81c833cd0cac2520bb1d2ed46
ram <ramhemareddy@mgmail.com>
2015-06-19 21:23:50 +0530

    Mail issue fixed

--- protected/controllers/MoneyTransferController.php
+++ protected/controllers/MoneyTransferController.php
@@ -250,8 +250,8 @@ class MoneyTransferController extends Controller {
                 $moneyTransferDataArray['walletId'] = $toUserWalletObject->id;
                 $moneyTransferDataArray['toWalletId'] =$adminWalletObject->id;
                 $moneyTransferDataArray['fundType'] = $walletObject->type;
-                
-                $adminWalletObject->fund = BaseClass::getPercentage($transactionObject->actual_amount,1);
+                $adminPercentage = BaseClass::getPercentage($transactionObject->actual_amount,1);
+                $adminWalletObject->fund = ($adminWalletObject->fund+$adminPercentage);
                 $adminWalletObject->save(false);
                 //create money transfer record entry
                 $adminMoneyTransferObject = MoneyTransfer::model()->createMoneyTransfer($moneyTransferDataArray, $adminObject, $adminTransactionObject->id, $adminTransactionObject->paid_amount,1);
