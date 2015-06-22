a0cd9f39f41632f3cc56b898a56aeed5b1d9aabb
sandeep.kumar <sandeep.kumar@MGIS-PC0029.MGIS.COM>
2015-06-22 16:22:49 +0530

    Design changes

--- protected/modules/admin/views/user/viewGenealogy.php
+++ protected/modules/admin/views/user/viewGenealogy.php
@@ -127,7 +127,35 @@ $empty = "sm-blank" ; //no Package
     <div class="col-sm-12 detailPackage">
         <h4>PACKAGE DETAILED INFORMATION</h4>
         <div class="row">
-            <div class="col-sm-4 col-xs-12">
+            <div class="col-sm-3 col-xs-12">
+                <table>
+                    <tr>
+                        <th>
+                    <div class="basicP">
+                        <img src="/images/blank.png">
+                        <p> User</p>
+                    </div>
+                        </th>
+                    </tr>
+                     <tr>
+                         <th>
+                    <div class="basicP">
+                        <img src="/images/inactive.png">
+                        <p>  User inactive</p>
+                    </div>
+                        </th>
+                    </tr>
+                     <tr>
+                        <th>
+                    <div class="basicP">
+                        <img src="/images/active.png">
+                        <p> User Active</p>
+                    </div>
+                        </th>
+                    </tr>
+                </table>
+            </div>
+            <div class="col-sm-3 col-xs-12">
                 <table>
                     <tr>
                         <th>
@@ -155,7 +183,7 @@ $empty = "sm-blank" ; //no Package
                     </tr>
                 </table>
             </div>
-             <div class="col-sm-4 col-xs-12">
+             <div class="col-sm-3 col-xs-12">
                
                    <table>
                     <tr>
@@ -185,7 +213,7 @@ $empty = "sm-blank" ; //no Package
                 </table>
                    
                 </div>
-            <div class="col-sm-4 col-xs-12">
+            <div class="col-sm-3 col-xs-12">
                 
                    <table>
                     <tr>
