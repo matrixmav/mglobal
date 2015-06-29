800005c64ca92bc2eefa1cccf30b5ae0db584e2c
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-06-29 10:54:42 +0530

    update

--- protected/views/package/wallethankyou.php
+++ protected/views/package/wallethankyou.php
@@ -1,8 +1,7 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div class="container">
+    <div class="col-lg-12">    
+        <div id="maincontent" class="pageWrp checkout abtest">
+            <div class="success"><?php echo $successMsg;?></div>
+       </div>
+    </div>
+</div>
