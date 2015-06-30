825b7242d4c4ad3be916174d4518a0d53b5394b6
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-06-30 12:56:00 +0530

    update

---
+++ protected/modules/admin/controllers/GenealogyController.php
@@ -0,0 +1,58 @@
+<?php
+
+class GenealogyController extends Controller {
+
+    /**
+     * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+     * using two-column layout. See 'protected/views/layouts/column2.php'.
+     */
+    public $layout = 'inner';
+
+    public function init() {
+        BaseClass::isLoggedIn();
+    }
+
+    /**
+     * @return array action filters
+     */
+    public function filters() {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+            'postOnly + delete', // we only allow deletion via POST request
+        );
+    }
+
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules() {
+        return array(
+            array('allow', // allow all users to perform 'index' and 'view' actions
+                'actions' => array('index', 'view','binarycalc'),
+                'users' => array('*'),
+            ),
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                'actions' => array('create', 'update'),
+                'users' => array('@'),
+            ),
+            array('allow', // allow admin user to perform 'admin' and 'delete' actions
+                'actions' => array('admin', 'delete'),
+                'users' => array('admin'),
+            ),
+            array('deny', // deny all users
+                'users' => array('*'),
+            ),
+        );
+    }
+    public function actionBinaryCalc() {
+        $adminId = 1;
+        $parentObject = Genealogy::model()->findByAttributes(array('user_id' => $adminId)); 
+        $parentObject = BaseClass::setPurchaseNode($parentObject);
+        if($parentObject){
+            echo "<script>alert('Binary Generarted Successfully.');return false;</script>";
+        }
+    }
+}
+?>
