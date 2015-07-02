ef86dfc8e3c56449887c62e0ed782df6c86aaddb
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-07-02 17:13:53 +0530

    update

--- protected/views/transaction/list.php
+++ protected/views/transaction/list.php
@@ -81,7 +81,7 @@ $this->breadcrumbs = array(
                  array(
                     'name' => 'id',
                     'header' => '<span style="white-space: nowrap;">Name &nbsp; &nbsp; &nbsp;</span>',
-                    'value' => 'isset($data->user->full_name)?$data->user->full_name:""',
+                    'value' => 'isset($data->user->name)?$data->user->name:""',
                 ),
                  /*array(
                     'name' => 'id',
