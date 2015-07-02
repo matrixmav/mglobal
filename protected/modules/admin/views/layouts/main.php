1f9c7e53295be96b332bde00b46cc6446a45dffb
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-07-02 15:38:49 +0530

    update

--- protected/modules/admin/views/layouts/main.php
+++ protected/modules/admin/views/layouts/main.php
@@ -912,8 +912,55 @@ License: You must have a valid license purchased only from themeforest(the above
                                 </li>
                                 <?php
                             }
+                             if (in_array('generatebinary', $accessArr)) {
+                             $reservation_pmenu = 9;
+                            if ((in_array($reservation_pmenu, $menusections ['psections'])) || (in_array($reservation_pmenu, $menusections ['section_ids']))) {
+                                $reservation_subsection = array(
+                                    
+                                    "Wallet/fundwallet" => "Cash Wallet",
+                                    "Wallet/rpwallet" => "RP Wallet",
+                                    "Wallet/commisionwallet" => "Commission Wallet",
+                                   
+                                );
+                                ?>
+                                
+                                <li
+                                    class="<?php echo (($curAction == 'fundwallet') || ($curAction == 'rpwallet') || ($curAction == 'commisionwallet')) ? "active" : ''; ?>">
+                                    <a href="javascript:;"> <i class="fa fa-user"></i>
+                                        <span class="title">Summary</span>
+                                        <span class="selected"></span> <span
+                                            class="arrow <?php echo (($curAction == 'fundwallet') || ($curAction == 'rpwallet') || ($curAction == 'commisionwallet')) ? "open" : ''; ?>">
+                                        </span>
+                                    </a>
+
+                                    <?php
+                                    echo '<ul class="sub-menu">';
+                                    foreach ($reservation_subsection as $ctName => $ctTitle) {
+//                                        if (in_array($ctTitle, $menusections ['sections'])) {
+                                            if ($ctName == "search/create") {
+                                                $ctName = "search/create/type/details";
+                                            }
+                                        if ($curAction == "resetpassword" && $curControllerLower == "user")
+                                                $class_content = 'class="active"';
+                                            else
+                                                $class_content = ($curControllerLower . "/" . $curActionLower == $ctName) ? 'class="active"' : '';
+
+                                            echo '<li ' . $class_content . '>';
+                                        echo '<a href="/admin/' . $ctName . '">' . Yii::t('translation', $ctTitle) . '</a>';
+                                            echo '</li>';
+                                            if ($ctName == "search/create/type/details") {
+                                                $ctName = "search/create";
+                                            }
+//                                        }
+                                    }
+                                    echo '</ul>';
+                                    ?>			
+                                </li>
+                                <?php
+                             }
                              
-                            
+                            }
+                             
                             $bases_pmenu = 4; ?>
                             
                     </ul>
