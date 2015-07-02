f976ac3cee139b1dfd8ab3db6d11b8b834ad096f
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-07-02 14:09:37 +0530

    update

--- protected/config/main.php
+++ protected/config/main.php
@@ -110,7 +110,7 @@ return array(
                 */
 		'db'=>array(
 
-				'connectionString' => 'mysql:host=localhost;dbname=mgloball_demo',
+				'connectionString' => 'mysql:host=localhost;dbname=mglobal',
 				'emulatePrepare' => true,
 				'username' => 'root',
 				'password' => '',
