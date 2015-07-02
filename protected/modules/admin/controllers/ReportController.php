8752fba0a3119c2ee4fd4c77d0e8ad1119f6fbe7
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-07-02 15:58:12 +0530

    update

--- protected/modules/admin/controllers/ReportController.php
+++ protected/modules/admin/controllers/ReportController.php
@@ -180,25 +180,25 @@ class ReportController extends Controller {
         
     }
     
-    public function actionTrackRefferal($param) {
+    public function actionTrackRefferal() {
        $error = "";
         $success = "";
         $todayDate = date('Y-m-d');
         $fromDate = date('Y-m-d');
-        $loggedInUserId = Yii::app()->session['userid'];
-        $userObject = User::model()->findByPK($loggedInUserId);
+        //$loggedInUserId = Yii::app()->session['userid'];
+        //$userObject = User::model()->findByPK($loggedInUserId);
         $pageSize = 100;
         if (!empty($_POST)) {
             $todayDate = $_POST['from'];
             $fromDate = $_POST['to'];
             $dataProvider = new CActiveDataProvider('User', array(
                 'criteria' => array(
-                    'condition' => ('sponsor_id="' . $userObject->name . '" AND social != "" AND created_at >= "' . $todayDate . '" AND created_at <= "' . $fromDate . '"'), 'order' => 'id DESC',
+                    'condition' => ('social != "" AND created_at >= "' . $todayDate . '" AND created_at <= "' . $fromDate . '"'), 'order' => 'id DESC',
             )));
         } else {
             $dataProvider = new CActiveDataProvider('User', array(
                 'criteria' => array(
-                    'condition' => ('sponsor_id="' . $userObject->name . '" AND social != "" AND created_at >= "' . $todayDate . '" AND created_at <= "' . $fromDate . '"'), 'order' => 'id DESC',
+                    'condition' => ('social != "" AND created_at >= "' . $todayDate . '" AND created_at <= "' . $fromDate . '"'), 'order' => 'id DESC',
             )));
         }
         $this->render('/report/admintrack_refferal', array(
