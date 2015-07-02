b9956f723458a135f27a3aad85b7afa3280d2131
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-07-02 10:44:14 +0530

    update

--- protected/views/package/domainsearch.php
+++ protected/views/package/domainsearch.php
@@ -22,21 +22,21 @@
                         
             <div class="row bs-wizard" style="border-bottom:0;">
                 
-                <div class="col-xs-4 bs-wizard-step complete">
+                <div class="col-xs-4 bs-wizard-step active">
                   <div class="text-center bs-wizard-stepnum">Step 1</div>
                   <div class="progress"><div class="progress-bar"></div></div>
                   <a href="#" class="bs-wizard-dot"></a>
                   <div class="bs-wizard-info text-center">Domain Search.</div>
                 </div>
                 
-                <div class="col-xs-4 bs-wizard-step complete"><!-- complete -->
+                <div class="col-xs-4 bs-wizard-step "><!-- complete -->
                   <div class="text-center bs-wizard-stepnum">Step 2</div>
                   <div class="progress"><div class="progress-bar"></div></div>
                   <a href="#" class="bs-wizard-dot"></a>
                   <div class="bs-wizard-info text-center">Proceed Payment</div>
                 </div>
                 
-                <div class="col-xs-4 bs-wizard-step active"><!-- complete -->
+                <div class="col-xs-4 bs-wizard-step "><!-- complete -->
                   <div class="text-center bs-wizard-stepnum">Step 3</div>
                   <div class="progress"><div class="progress-bar"></div></div>
                   <a href="#" class="bs-wizard-dot"></a>
