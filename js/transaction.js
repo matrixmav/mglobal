f5e9af02ea39fcecfc83191c6b4f634346bcfa2c
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-07-03 18:19:04 +0530

    update

--- js/transaction.js
+++ js/transaction.js
@@ -1,22 +1,24 @@
-function getExistingFund(userId,walletId){ 
-   // alert(userId);return false;
-        $.ajax({
-            type: "post",
-            url: "/admin/wallet/getfundbyamount",
-            data: {'userId':userId,'walletId':walletId},
-            success: function (amount) { 
-                $("#transaction_data").html("");
-                if(amount != 0){
-                    $("#transaction_data").html("<b>"+amount+"</b>");
-                    
-                } else {
-                    $("#transaction_data").html("<b>0.00</b>");
-                }
-            }
-        });
-    }
+//function getExistingFund(userId,walletId){ 
+//   // alert(userId);return false;
+//        $.ajax({
+//            type: "post",
+//            url: "/admin/wallet/getfundbyamount",
+//            data: {'userId':userId,'walletId':walletId},
+//            success: function (amount) { 
+//                $("#transaction_data").html("");
+//                if(amount != 0){
+//                    $("#transaction_data").html("<b>"+amount+"</b>");
+//                    
+//                } else {
+//                    $("#transaction_data").html("<b>0.00</b>");
+//                }
+//            }
+//        });
+//    }
 function getExistingFund(userId, walletId) {
-    //alert(userId);return false;
+//    alert(userId);
+//    alert(walletId);
+//    return false;
     $.ajax({
         type: "post",
         url: "/admin/wallet/getfundbyamount",
