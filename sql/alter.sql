545f10003d1406a4d11b53da6df4f8bcb8dc4597
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-07-03 11:55:20 +0530

    update

--- sql/alter.sql
+++ sql/alter.sql
@@ -170,4 +170,7 @@ CREATE TABLE IF NOT EXISTS `user_has_coupon` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 
+
 ALTER TABLE `transaction` ADD `coupon_code` VARCHAR(100) NOT NULL AFTER `coupon_discount`;
+
+ALTER TABLE `user_has_coupon` CHANGE `coupon_code` `coupon_id` INT(11) NOT NULL;
