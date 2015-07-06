77e3b65c4c89c16c33aa3f77cb2efd20752cdb89
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-07-06 14:49:56 +0530

    update

--- protected/models/User.php
+++ protected/models/User.php
@@ -156,21 +156,7 @@ class User extends CActiveRecord
 		));
 	}
         
-        public function binaryMail($parentObject) {
-        
-         /*code to send mail for binary commission*/  
-                $userObject = User::model()->findByPk($parentObject->user_id);
-                $userObjectArr = array();
-                $userObjectArr['to_name'] = $userObject->full_name;
-                $userObjectArr['user_name'] = $userObject->name;
-                $config['to'] = $userObject->email;
-                $config['subject'] = 'Binary Income Credited';
-                $config['body'] =  $this->renderPartial('//mailTemp/binary_commission', array('userObjectArr'=>$userObjectArr),true);
-                CommonHelper::sendMail($config);
-        
-                }
-        
-
+       
 	/**
 	 * Returns the static model of the specified AR class.
 	 * Please note that you should have this exact method in all your CActiveRecord descendants!
