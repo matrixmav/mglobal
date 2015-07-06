5beb3673a85eacadbcf526f78181bb0f808884e6
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-07-06 13:23:22 +0530

    update

--- protected/components/BaseClass.php
+++ protected/components/BaseClass.php
@@ -1382,7 +1382,7 @@ class BaseClass extends Controller {
             $postDataArray['fromUserId'] = 1;
             $postDataArray['comment'] = 'Binary Commission Transfered.'; 
             $moneyTransferObject = MoneyTransfer::model()->createMoneyTransfer($postDataArray, $userObject, $transactionObjectect->id, $transactionObjectect->paid_amount,'admin');  
-             
+            $mailSent = User::model()->BinaryMail($parentObject);
             return 1;   
             
             }
