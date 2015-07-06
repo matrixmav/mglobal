0727f64538754d913a455e8cbe7a21f99da3097f
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-07-06 14:50:31 +0530

    update

--- protected/modules/admin/controllers/UserController.php
+++ protected/modules/admin/controllers/UserController.php
@@ -112,7 +112,21 @@ class UserController extends Controller {
     }
     
    
-    
+     public function binaryMail($parentObject) {
+        
+         /*code to send mail for binary commission*/  
+                $userObject = User::model()->findByPk($parentObject->user_id);
+                $userObjectArr = array();
+                $userObjectArr['to_name'] = $userObject->full_name;
+                $userObjectArr['user_name'] = $userObject->name;
+                $config['to'] = $userObject->email;
+                $config['subject'] = 'Binary Income Credited';
+                $config['body'] =  $this->renderPartial('//mailTemp/binary_commission', array('userObjectArr'=>$userObjectArr),true);
+                CommonHelper::sendMail($config);
+        
+       }
+        
+
    
     public function actionGenealogy() {
         $emailObject = User::model()->findAll(array('condition' => 'sponsor_id = "admin"'));
