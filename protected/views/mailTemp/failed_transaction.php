639bad0c8f11aa92c1053253a9ba6fdd89cb2809
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-07-06 10:22:50 +0530

    update

---
+++ protected/views/mailTemp/failed_transaction.php
@@ -0,0 +1,30 @@
+<!-- content -->
+ <?php $this->renderPartial('/mailTemp/header'); ?>   
+    <tr>
+    	   <td valign="" bgcolor="#efed6a" height="55" align="left" style="line-height:0px; font-size:16px">
+        	<table width="100%" cellspacing="0" cellpadding="0" border="0">
+          <tbody>
+            <tr>
+            	<td width="5%" valign="middle" align="center" style="line-height:22px; color: #828282; font-size:16px; font-family:'Nunito'"> </td>
+                <td width="90%" valign="middle" align="left" style="line-height:22px; color: #828282; font-size:16px; font-family:'Nunito'"> Hello Sandeep</td>
+                <td width="5%" valign="middle" align="center" style="line-height:22px; color: #828282; font-size:16px; font-family:'Nunito'"> </td>
+            </tr>
+            </tbody>
+            </table>
+        </td>
+    </tr>
+    
+    <!-- text description -->
+    <tr>
+    	   <td valign="" bgcolor="#fafafa" height="" align="left" style="line-height:0px; font-size:16px">
+        	<table width="100%" cellspacing="0" cellpadding="0" border="0">
+          <tbody>
+            <tr>
+            	<td width="5%" valign="middle" align="center" style="line-height:22px; color: #828282; font-size:16px; font-family:'Nunito'"> </td>
+                <td width="90%" valign="middle" align="left" style="line-height:22px; color: #828282; font-size:16px; font-family:'Nunito'"> 
+                <table width="100%" cellspacing="0" cellpadding="0" border="0">
+          <tbody>
+            <tr>
+            	  <td height="20" bgcolor="" style=""></td>
+            </tr>
+             <?php $this->renderPartial('/mailTemp/footer'); ?>
