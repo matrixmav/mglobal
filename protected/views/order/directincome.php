b8011c36b53515471b129699f02f244f5a590125
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-07-10 13:07:03 +0530

    update

--- protected/views/order/directincome.php
+++ protected/views/order/directincome.php
@@ -29,10 +29,10 @@ $this->menu = array(
                     
     <form id="regervation_filter_frm" name="regervation_filter_frm" method="post" action="/order/refferalincome">
     <div class="input-group input-large date-picker input-daterange">
-        <input type="text" name="from" placeholder="To Date" class="datepicker form-control">
+        <input type="text" name="from" placeholder="To Date" class="datepicker form-control" value="<?php echo date("Y-m-d", mktime(0, 0, 0, date("m") , date("d")-1,date("Y")));?>">
         <span class="input-group-addon">
         to </span>
-        <input type="text" name="to" data-provide="datepicker" placeholder="From Date" class="datepicker form-control">
+        <input type="text" name="to" data-provide="datepicker" placeholder="From Date" class="datepicker form-control" value="<?php echo date('Y-m-d');?>">
     </div>
     <?php 
     /*$statusId =   1;
