1c2d3bb0250bef00d93ec2e606b95be18986b968
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-07-14 16:32:46 +0530

    update

--- protected/views/order/pendingPayment.php
+++ protected/views/order/pendingPayment.php
@@ -196,7 +196,7 @@
                             <input type="hidden" name="currency_code" value="USD">
                             <input type="hidden" name="handling" value="0">
                             <input type="hidden" name="cancel_return" value="">
-                            <input type="hidden" id="cancel_return" name="cancel_return" value="<?php echo Yii::app()->params['returnurl']; ?>?transaction_id=<?php if(!empty($transactionObject)) { echo $transactionObject->transaction_id;} ?>&status=cancel">
+                            <input type="hidden" id="cancel_return" name="cancel_return" value="<?php echo Yii::app()->params['returnurl']; ?>?transaction_id=<?php if(!empty($transactionObject)) { echo $transactionObject->transaction_id;} ?>&status=cancelled">
                             <input type="hidden" id="return" name="return" value="<?php echo Yii::app()->params['returnurl']; ?>?transaction_id=<?php if(!empty($transactionObject)) { echo $transactionObject->transaction_id;} ?>&status=success">
                         </form>
                     </div>
