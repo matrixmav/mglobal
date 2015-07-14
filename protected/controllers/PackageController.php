ccec48924fd5fe51979ba6632906f5459fcba4b3
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-07-14 16:54:38 +0530

    update

--- protected/controllers/PackageController.php
+++ protected/controllers/PackageController.php
@@ -825,7 +825,7 @@ class PackageController extends Controller {
             $transactionId = $_GET['transaction_id'];
             $transactionObject = Transaction::model()->findByAttributes(array('transaction_id' => $transactionId));
             $userObject = User::model()->findByPK(Yii::app()->session['userid']);
-	    if($transactionObject->status == 0) {
+	    if($transactionObject->status == 1) {
             $transactionObject->status = 1;
              $transactionObject->created_at = date('Y-m-d');
             $transactionObject->update();
