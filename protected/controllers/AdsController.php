4bdab155a29f82784cef4c605dc1c250c6b19e10
Ram Hemareddy <ramhemareddy@gmail.com>
2015-07-22 10:20:40 +0530

    Template

--- protected/controllers/AdsController.php
+++ protected/controllers/AdsController.php
@@ -56,7 +56,7 @@ class AdsController extends Controller {
         ));
     }
 
-    public function getSocialButton($data, $row) {        
+    public function getSocialButton($data) {        
         $this->renderPartial('shareoptions', array('data' => $data), false, true);
        
     }
@@ -162,7 +162,7 @@ class AdsController extends Controller {
      * Lists all models.
      */
     public function actionIndex() {
-
+        $dataProviderArray ="";
         $successMsg = "";
         $orderObject = Order::model()->findAll(array('condition' => 'user_id = '. Yii::app()->session['userid']));
         foreach($orderObject as $orderObjectList){           
