b0679800f5a18382dc42b0caecfe341e514702c2
Nidhi Sati <nidhi.sati@maverickinfosoft.com>
2015-07-23 16:54:18 +0530

    updated

--- protected/controllers/ProfileController.php
+++ protected/controllers/ProfileController.php
@@ -104,6 +104,7 @@ class ProfileController extends Controller {
                 if (md5($_POST['UserProfile']['master_pin']) == $userObject->master_pin) {
 
                     $profileObject->testimonials = $_POST['UserProfile']['testimonials'];
+                    $profileObject->updated_at = new CDbExpression('NOW()');
                     $profileObject->testimonial_status = '0';
 
                     if ($profileObject->update()) {
@@ -185,6 +186,7 @@ class ProfileController extends Controller {
         if ($_POST && $_FILES['id_proof']['name'] != '' && $_FILES['address_proof']['name'] != '') {
             $profileObject->id_proof = time() . $_FILES['id_proof']['name'];
             $profileObject->address_proff = time() . $_FILES['address_proof']['name'];
+            $profileObject->updated_at = new CDbExpression('NOW()');
             if ($_FILES) {
                 if (md5($_POST['UserProfile']['master_pin']) == $userObject->master_pin) {
                     $ext1 = end((explode(".", $profileObject->id_proof)));
