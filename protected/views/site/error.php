96b1d1009bc060d2ef1017243a6888e32301faf3
ajay.kumar <ajay.kumar@MGIS-PC0012.MGIS.COM>
2015-07-23 16:46:09 +0530

    Merge branch 'master' of https://github.com/matrixmav/mglobal
    Conflicts:
    	protected/views/user/404.php

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -8,7 +8,12 @@
         font-size:85px;
         font-family:"PT Sans Narrow";
         color:#E54A1A;
+        text-align: center;
     }
+     .not-found img
+     {
+         width:35%;
+     }
     .not-found h2
     {
         font-size:50px;
@@ -20,14 +25,30 @@
         font-size:25px;
         font-family:"PT Sans Narrow";
         color:#E54A1A;
+        text-align: center;
+        line-height: 2;
+    }
+    @media screen and (max-width : 768px)
+    {
+        .not-found h2
+    {
+        font-size:35px;
+        font-family:"PT Sans Narrow";
+        color:#E54A1A;
+        text-align: center;
+    }
+     .not-found img
+     {
+         width:75%;
+     }
     }
 </style>
 <div class="container">
     <div class="col-md-12">
         <div class="not-found">
             <h2>OOPS! SOMETHING WENT WRONG!</h2>
-            <h1>404</h1>
-            <h2>Page Not Found</h2>
+            <h1><img src="../../../images/404.png"></h1>
+            
             <p>It looks like that page no longer exists. Would you like to go to <a href="#">homepage</a> instead?</p>
         </div>
     </div>
