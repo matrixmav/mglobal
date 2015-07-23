9b17f0f9e00d13dc9708b24b0a91956cad669457
Ram Hemareddy <ramhemareddy@gmail.com>
2015-07-23 16:39:39 +0530

    Template

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -1,49 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title></title>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/bootstrap.css">
-        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-        <link href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
-    </head>
-    <style>
-        .not-found
-        {
-            text-align:center;
-        }
-        .not-found h1
-        {
-            font-size:85px;
-            font-family:"PT Sans Narrow";
-            color:#E54A1A;
-        }
-        .not-found h2
-        {
-            font-size:50px;
-            font-family:"PT Sans Narrow";
-            color:#E54A1A;
-        }
-        .not-found p
-        {
-            font-size:25px;
-            font-family:"PT Sans Narrow";
-            color:#E54A1A;
-        }
-    </style>
-    <body>
-        <div class="container">
-            <div class="col-md-12">
-                <div class="not-found">
-                    <h2>OOPS! SOMETHING WENT WRONG!</h2>
-                    <h1>404</h1>
-                    <h2>Page Not Found</h2>
-                    <p>It looks like that page no longer exists. Would you like to go to <a href="#">homepage</a> instead?</p>
-                </div>
-            </div>
+<style>
+    .not-found
+    {
+        text-align:center;
+    }
+    .not-found h1
+    {
+        font-size:85px;
+        font-family:"PT Sans Narrow";
+        color:#E54A1A;
+    }
+    .not-found h2
+    {
+        font-size:50px;
+        font-family:"PT Sans Narrow";
+        color:#E54A1A;
+    }
+    .not-found p
+    {
+        font-size:25px;
+        font-family:"PT Sans Narrow";
+        color:#E54A1A;
+    }
+</style>
+<div class="container">
+    <div class="col-md-12">
+        <div class="not-found">
+            <h2>OOPS! SOMETHING WENT WRONG!</h2>
+            <h1>404</h1>
+            <h2>Page Not Found</h2>
+            <p>It looks like that page no longer exists. Would you like to go to <a href="#">homepage</a> instead?</p>
         </div>
-  
+    </div>
+</div>
